0f27245fb4a0b67f042fee63954f200cb824a881
robnagler <github@q33.us>
2015-10-22 01:25:45 +0000

    added synergia install NERSC host hopper

---
+++ bin/hopper-synergia.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# Install synergia on hopper
+#
+hopper_synergia_main() {
+    install_info 'Loading modules (ignore error about hdf5)'
+    hopper_synergia_modules='
+        module unload darshan
+        # Only for build??
+        module unload cray-shmem
+        module unload xt-asyncpe
+        module switch PrgEnv-pgi PrgEnv-gnu
+        module load cray-hdf5
+        module load craype
+        module load gsl
+        module load cmake
+        module load git
+        module load python/2.7.9
+        module load pytables
+        # pytables loads hdf5 which is deprecated and conflicts with cray-hdf5
+        module unload hdf5
+        module load mpi4py
+        module load boost
+        module load fftw
+        # "module load atp" does not set PKG_CONFIG_PATH automatically
+        if [[ ! :$PKG_CONFIG_PATH: =~ :$ATP_HOME/lib/pkgconfig: ]]; then
+            export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$ATP_HOME/lib/pkgconfig
+        fi;'
+    eval "$hopper_synergia_modules"
+    git clone -q http://cdcvs.fnal.gov/projects/contract-synergia2
+    cd contract-synergia2
+    git checkout -b devel origin/devel
+    ./bootstrap
+    # the test is wrong for hopper; Need to set -DFFTW3_LIBRARY_DIRS explicitly
+    perl -pi -e 's{^if fftw3.*}{if True:}' packages/{chef_libs,synergia2}.py
+    # Spelling of INCLUDEDIR is wrong.
+    # This patches the CMakeLists.txt multiple times, because there's no "post_git" command. contractor executes the
+    # packages every time is starts. Very strange. Anyway, the FIND_PACKAGE(MPI required) is wrong. We don't
+    # need it, because we specify the compiler explicitly.
+    perl -pi -e '
+        s{Boost_INCLUDE_DIR}{BOOST_INCLUDEDIR:PATH};
+        /Git_clone/ && s{$}{;import subprocess,os;subprocess.call(["perl", "-pi", "-e", "/^FIND_.*MPI/ && s{^}{#}", "build/synergia2/CMakeLists.txt"], stderr=open(os.devnull, "w"))}
+        ' packages/synergia2.py
+    # Temporary, because sourceforge was barking
+    perl -pi -e 's{http://source.*}{https://depot.radiasoft.org/foss/pyparsing-1.5.5.tar.gz"}' packages/pyparsing_pkg.py
+    # Basic configuration seems to work. Requires a number of environment variables to be set (extra_envs)
+    ./contract.py --configure-import configs/synergia_config_hopper
+    # Barnacle.hpp fails to be found (sometimes) if you parallel build chef-libs
+    # Not sure if all these extra_envs are needed, but environment variables aren't automatically
+    # passed through. We use our patched chef-libs, because the default one is broken.
+    ./contract.py --configure \
+        "extra_envs=GCC_PATH=$GCC_PATH,FFTW_DIR=$FFTW_DIR,FFTW_INC=$FFTW_INC,ASYNCPE_DIR=/opt/cray/craype/default,BOOST_DIR=$BOOST_DIR,MPICH_DIR=$MPICH_DIR,GSL_DIR=$GSL_DIR" \
+        chef-libs/repo=https://github.com/radiasoft/accelerator-modeling-chef.git \
+        chef-libs/branch=radiasoft-devel \
+        boost/lib="$BOOST_DIR"/libs \
+        boost/include="$BOOST_DIR"/boost \
+        chef-libs/make_use_custom_parallel=1 \
+        chef-libs/make_custom_parallel=1
+    ./contract.py
+    echo "$hopper_synergia_modules" > setup.d/00-modules.sh
+    install_log "Done. Don't forget to ./setup.sh to setup the environment"
+}
+
+hopper_synergia_main
--- bin/install.sh
+++ bin/install.sh
@@ -5,6 +5,9 @@
 set -e
 
 install_check() {
+    if [[ $install_no_check ]]; then
+        return
+    fi
     if [[ $(ls -A | grep -v install.log) ]]; then
         install_err 'Current directory is not empty.
 Please create a new directory, cd to it, and re-run this command.'
@@ -74,8 +77,8 @@ install_main() {
     install_log_file=$PWD/install.log
     trap install_err_trap ERR
     install_log install_main
-    install_check
     install_vars "$@"
+    install_check
     eval "$(install_download $install_type.sh)"
 }
 
@@ -85,38 +88,48 @@ install_msg() {
 
 install_usage() {
     install_err "$@
-usage: $(basename $0) [vagrant|docker] beamsim|python2|sirepo"
+usage: $(basename $0) [docker|hopper|vagrant] beamsim|python2|sirepo|synergia"
 }
 
 install_vars() {
-    case "$(uname)" in
-        [Dd]arwin)
-            if [[ $(type -t vagrant) ]]; then
-                install_type=vagrant
-            else
-                install_err 'Please install Vagrant and restart install'
-            fi
-            ;;
-        [Ll]inux)
-            if [[ $(type -t docker) ]]; then
-                install_type=docker
-            elif [[ $(type -t vagrant) ]]; then
-                install_type=vagrant
-            else
-                install_err 'Please install Docker or Vagrant and restart install'
-            fi
-            ;;
-        *)
-            install_err "$(uname) is an unsupported system, sorry"
-            ;;
-    esac
+    if [[ hopper == $NERSC_HOST ]]; then
+        install_type=hopper
+    else
+        case "$(uname)" in
+            [Dd]arwin)
+                if [[ $(type -t vagrant) ]]; then
+                    install_type=vagrant
+                else
+                    install_err 'Please install Vagrant and restart install'
+                fi
+                ;;
+            [Ll]inux)
+                if [[ $(type -t docker) ]]; then
+                    install_type=docker
+                elif [[ $(type -t vagrant) ]]; then
+                    install_type=vagrant
+                else
+                    install_err 'Please install Docker or Vagrant and restart install'
+                fi
+                ;;
+            *)
+                install_err "$(uname) is an unsupported system, sorry"
+                ;;
+        esac
+    fi
     install_image=
     install_forward_port=
     install_verbose=
     while [[ "$1" ]]; do
         case "$1" in
             beamsim|python2|sirepo)
-                install_image=radiasoft/$1
+                install_image=$1
+                ;;
+            hopper)
+                install_type=$1
+                ;;
+            synergia)
+                install_image=$1
                 ;;
             vagrant|docker)
                 install_type=$1
@@ -134,14 +147,35 @@ install_vars() {
         shift
     done
     if [[ ! $install_image ]]; then
-        install_image=radiasoft/$(basename "$PWD")
-        if [[ ! $install_image =~ ^radiasoft/(beamsim|python2|sirepo)$ ]]; then
-            install_usage "Please supply a container name: beamsim, python2, or sirepo"
+        install_image=$(basename "$PWD")
+        if [[ ! $install_image =~ ^(beamsim|python2|sirepo)$ ]]; then
+            install_usage "Please supply a install name: beamsim, python2, sirepo, synergia"
         fi
     fi
-    if [[ $install_image == radiasoft/sirepo ]]; then
+    if [[ $install_image =~ sirepo ]]; then
         install_forward_port=8000
     fi
+    case $install_type in
+        vagrant|docker)
+            if [[ $NERSC_HOST ]]; then
+                install_usage "You can't install vagrant or docker at NERSC"
+            fi
+            if [[ $install_image == synergia ]]; then
+                install_msg 'Switching image to "beamsim" which includes synergia'
+                install_image=beamsim
+            fi
+            install_image=radiasoft/$install_image
+            ;;
+        hopper)
+            install_no_check=1
+            if [[ $NERSC_HOST != hopper ]]; then
+                install_usage "You are not running on $install_type so can't install"
+            fi
+            if [[ $install_image != synergia ]]; then
+                install_usage "Can only install synergia on $install_type"
+            fi
+            ;;
+    esac
     install_url=https://raw.githubusercontent.com/radiasoft/download/master/bin
 }
 
