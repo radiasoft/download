5e7eaf33ee031979b7a09f9d637cabc4dcc626dd
robnagler <github@q33.us>
2015-10-22 01:29:10 +0000

    fpc

--- bin/hopper.sh
+++ bin/hopper.sh
@@ -4,7 +4,7 @@
 #
 hopper_main() {
     install_info "Installing $install_image on $install_type"
-    eval $(install_download "$install_type-$install_image.sh")
+    eval "$(install_download "$install_type-$install_image.sh")"
 }
 
-hopper_main
+hopper_main "$@"
