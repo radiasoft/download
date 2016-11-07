ad9a575d515b483eb0ed329092a4d20ed97e1e92
robnagler <github@q33.us>
2016-11-07 23:27:22 +0000

    install-from-git installer

---
+++ installers/init-from-git/radiasoft-download.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# To run: curl radia.run | bash -s init-from-git repo...
+#
+# The file 'radia-run.sh' will be run in the cloned repo.
+#
+# repo may be https://github.com/foo/bar or foo/bar
+
+init_from_git_main() {
+    install_tmp_dir
+    init_from_git_tmpdir=$(pwd)
+    cd
+    # Avoid false returns
+    set -e
+    . ~/.bashrc
+    set +e
+    for repo in "${install_extra_args[@]}"; do
+        init_from_git_one "$repo"
+    done
+}
+
+init_from_git_one() {
+    local repo=$1
+    rm -rf "$init_from_git_tmpdir"
+    mkdir -p "$init_from_git_tmpdir"
+    (
+        set -e
+        cd "$init_from_git_tmpdir"
+        if [[ ! repo =~ ^\w+:// ]]; then
+            repo=https://github.com/$repo
+        fi
+        git clone -q "$repo"
+        cd "$(basename "$repo" .git)"
+        . ./radia-run.sh
+    ) || true
+}
+
+init_from_git_main
