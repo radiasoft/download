7b1eaa76f2353238742fef1af67b276991a320b5
robnagler <github@q33.us>
2017-07-26 13:07:49 +0000

    split out trilinos

--- installers/code/codes/opal.sh
+++ installers/code/codes/opal.sh
@@ -11,56 +11,18 @@ CC=mpicc CXX=mpicxx ./configure \
   --prefix="$(pyenv prefix)" \
   --with-pic
 #  --enable-shared
-make install
+make -j 2 install
 cd "$base_pwd"
 
 #http://glaros.dtc.umn.edu/gkhome/metis/parmetis/download
 codes_download_foss parmetis-4.0.3.tar.gz
 make config shared=1 prefix="$(pyenv prefix)"
-make install
+make -j 2 install
 cd "$base_pwd"
 
 #http://glaros.dtc.umn.edu/gkhome/metis/metis/download
 codes_download_foss metis-5.1.0.tar.gz
 make config shared=1 prefix="$(pyenv prefix)"
-make install
-cd "$base_pwd"
-
-# https://trilinos.org/oldsite/download/download.html
-codes_download_foss trilinos-12.10.1-Source.tar.gz
-mkdir build
-cd build
-CC=mpicc CXX=mpicxx cmake \
-  -DCMAKE_INSTALL_PREFIX:PATH="$(pyenv prefix)" \
-  -DCMAKE_CXX_FLAGS:STRING="-DMPICH_IGNORE_CXX_SEEK -fPIC" \
-  -DCMAKE_C_FLAGS:STRING="-DMPICH_IGNORE_CXX_SEEK -fPIC" \
-  -DCMAKE_CXX_STANDARD:STRING="11" \
-  -DCMAKE_Fortran_FLAGS:STRING="-fPIC" \
-  -DCMAKE_BUILD_TYPE:STRING=Release \
-  -DMETIS_LIBRARY_DIRS="$(pyenv prefix)/lib" \
-  -DTPL_ENABLE_DLlib:BOOL=OFF \
-  -DTPL_ENABLE_QT:BOOL=OFF \
-  -DTPL_ENABLE_MPI:BOOL=ON \
-  -DTPL_ENABLE_BLAS:BOOL=ON \
-  -DTPL_ENABLE_LAPACK:BOOL=ON \
-  -DTPL_ENABLE_METIS:BOOL=ON \
-  -DTPL_ENABLE_ParMETIS:BOOL=ON \
-  -DTrilinos_ENABLE_Amesos:BOOL=ON \
-  -DTrilinos_ENABLE_Amesos2:BOOL=ON \
-  -DTrilinos_ENABLE_AztecOO:BOOL=ON \
-  -DTrilinos_ENABLE_Belos:BOOL=ON \
-  -DTrilinos_ENABLE_Epetra:BOOL=ON \
-  -DTrilinos_ENABLE_EpetraExt:BOOL=ON \
-  -DTrilinos_ENABLE_Galeri:BOOL=ON \
-  -DTrilinos_ENABLE_Ifpack:BOOL=ON \
-  -DTrilinos_ENABLE_Isorropia:BOOL=ON \
-  -DTrilinos_ENABLE_ML:BOOL=ON \
-  -DTrilinos_ENABLE_NOX:BOOL=ON \
-  -DTrilinos_ENABLE_Optika:BOOL=OFF \
-  -DTrilinos_ENABLE_Teuchos:BOOL=ON \
-  -DTrilinos_ENABLE_Tpetra:BOOL=ON \
-  -DTrilinos_ENABLE_TESTS:BOOL=OFF \
-  ..
 make install
 cd "$base_pwd"
 
@@ -78,5 +40,5 @@ CMAKE_PREFIX_PATH="$(pyenv prefix)" H5HUT_PREFIX="$(pyenv prefix)" \
     -DCMAKE_INSTALL_PREFIX="$(pyenv prefix)" \
     -DENABLE_SAAMG_SOLVER=TRUE \
     ..
-make install
+make -j 2 install
 cd "$base_pwd"
---
+++ installers/code/codes/trilinos.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# https://trilinos.org/oldsite/download/download.html
+codes_download_foss trilinos-12.10.1-Source.tar.gz
+mkdir build
+cd build
+CC=mpicc CXX=mpicxx cmake \
+  -DCMAKE_INSTALL_PREFIX:PATH="$(pyenv prefix)" \
+  -DCMAKE_CXX_FLAGS:STRING="-DMPICH_IGNORE_CXX_SEEK -fPIC" \
+  -DCMAKE_C_FLAGS:STRING="-DMPICH_IGNORE_CXX_SEEK -fPIC" \
+  -DCMAKE_CXX_STANDARD:STRING="11" \
+  -DCMAKE_Fortran_FLAGS:STRING="-fPIC" \
+  -DCMAKE_BUILD_TYPE:STRING=Release \
+  -DMETIS_LIBRARY_DIRS="$(pyenv prefix)/lib" \
+  -DTPL_ENABLE_DLlib:BOOL=OFF \
+  -DTPL_ENABLE_QT:BOOL=OFF \
+  -DTPL_ENABLE_MPI:BOOL=ON \
+  -DTPL_ENABLE_BLAS:BOOL=ON \
+  -DTPL_ENABLE_LAPACK:BOOL=ON \
+  -DTPL_ENABLE_METIS:BOOL=ON \
+  -DTPL_ENABLE_ParMETIS:BOOL=ON \
+  -DTrilinos_ENABLE_Amesos:BOOL=ON \
+  -DTrilinos_ENABLE_Amesos2:BOOL=ON \
+  -DTrilinos_ENABLE_AztecOO:BOOL=ON \
+  -DTrilinos_ENABLE_Belos:BOOL=ON \
+  -DTrilinos_ENABLE_Epetra:BOOL=ON \
+  -DTrilinos_ENABLE_EpetraExt:BOOL=ON \
+  -DTrilinos_ENABLE_Galeri:BOOL=ON \
+  -DTrilinos_ENABLE_Ifpack:BOOL=ON \
+  -DTrilinos_ENABLE_Isorropia:BOOL=ON \
+  -DTrilinos_ENABLE_ML:BOOL=ON \
+  -DTrilinos_ENABLE_NOX:BOOL=ON \
+  -DTrilinos_ENABLE_Optika:BOOL=OFF \
+  -DTrilinos_ENABLE_Teuchos:BOOL=ON \
+  -DTrilinos_ENABLE_Tpetra:BOOL=ON \
+  -DTrilinos_ENABLE_TESTS:BOOL=OFF \
+  ..
+make -j 2 install
+cd "$base_pwd"
