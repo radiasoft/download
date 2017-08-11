bba27aa2c038dff618807a2a1bf0e25eb959e9b8
robnagler <github@q33.us>
2017-08-11 19:55:26 +0000

    install_script_eval cannot have /

--- installers/centos7/radiasoft-download.sh
+++ installers/centos7/radiasoft-download.sh
@@ -6,11 +6,11 @@ centos7_main() {
     local op=$1
     shift
     install_extra_args=( "$@" )
-    install_url radiasoft/centos7
+    install_url radiasoft/centos7/script
     if [[ $op =~ / ]]; then
         install_err "$op: unknown operation"
     fi
-    install_script_eval "script/$(basename "$op" .sh).sh"
+    install_script_eval "$(basename "$op" .sh).sh"
 }
 
 centos7_main "${install_extra_args[@]}"
