b86febb3d60b782915d373a9d80c346739269510
robnagler <github@q33.us>
2017-08-11 09:45:25 +0000

    centos7 home-env configuration

---
+++ installers/centos7/home-env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Set up home-env for root and vagrant
+#
+
+home_env_main() {
+    if (( $EUID != 0 )); then
+        install_err 'must be run as root'
+    fi
+    yum install -y git emacs-nox
+    (
+        set -e -o pipefail
+        install_main home
+    )
+    if (( $? != 0 )); then
+        install_err 'root environment install failed'
+    fi
+    # Encapsulate this
+    su - vagrant <<EOF
+$(declare -f $(compgen -A function | grep '^install_'))
+install_main home
+EOF
+}
+
+home_env_main "${install_extra_args[@]}"
