3bacf6f9bd61e24cbb26ef3f67b7e11c05411d6b
robnagler <github@q33.us>
2017-08-11 19:29:28 +0000

    moved to radiasoft/centos7

--- installers/centos7/radiasoft-download.sh
+++ installers/centos7/radiasoft-download.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #
-# centos7 install
+# To run: curl radia.run | bash -s centos7 op ...
 #
 centos7_main() {
     local op=$1
     shift
     install_extra_args=( "$@" )
-    install_script_eval "$op.sh"
+    install_url radiasoft/centos7
+    if [[ $op =~ / ]]; then
+        install_err "$op: unknown operation"
+    fi
+    install_script_eval "script/$(basename "$op" .sh).sh"
 }
 
 centos7_main "${install_extra_args[@]}"
