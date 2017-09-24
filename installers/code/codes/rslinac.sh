55f6238b048f3e3342a8d42143a8a78093721295
robnagler <github@q33.us>
2017-09-24 18:19:46 +0000

    update to last known commit

--- installers/code/codes/rslinac.sh
+++ installers/code/codes/rslinac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 codes_yum install boost-devel
-codes_download radiasoft/rslinac integration
+# Working commit on integration branch
+codes_download radiasoft/rslinac b0c58164f8405a976a06a5f89e27808abc474307
 python setup.py install
