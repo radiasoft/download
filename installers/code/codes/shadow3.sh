c9fccbd8338f5580ee4f5e4bc9e1ff9645d04e53
robnagler <github@q33.us>
2017-10-05 11:40:23 +0000

    Shadow3 fixed again https://github.com/srio/shadow3/commit/afb9621fc2a027dea171bea5c0b992ea7e970e37

--- installers/code/codes/shadow3.sh
+++ installers/code/codes/shadow3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 codes_dependencies xraylib
-codes_download srio/shadow3 e745b4957c8931974634856e80ee0006dc5eb754
+codes_download srio/shadow3
 #TODO(robnagler) shadow3 doesn't include dependencies
 # codes_patch_requirements_txt
 python setup.py install
