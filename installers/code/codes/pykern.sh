6451d8c1b41b2abe7925e65b0f23d8fca63ce96a
robnagler <github@q33.us>
2017-10-06 21:18:22 +0000

    installation order of plugins doesn't affect registration order

--- installers/code/codes/pykern.sh
+++ installers/code/codes/pykern.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 codes_download pykern
-# https://github.com/pytest-dev/pytest/issues/935
 pip uninstall -y pykern >& /dev/null || true
-for f in pytest-xdist pytest-forked pykern; do
-    pip uninstall -y "$f" >& /dev/null || true
-done
 pip install -r requirements.txt
 python setup.py install
 pyenv rehash
