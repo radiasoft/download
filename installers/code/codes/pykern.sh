42e48252f9354969c60a7b4aafd28b51c178d46d
robnagler <github@q33.us>
2017-10-06 17:39:25 +0000

    See pytest-dev/pytest#935: plugin order is random Well, doesn't seem to be truly random in that the order the plugins are installed seems to matter

--- installers/code/codes/pykern.sh
+++ installers/code/codes/pykern.sh
@@ -4,3 +4,10 @@ pip install -r requirements.txt
 python setup.py install
 pyenv rehash
 hash pykern
+# https://github.com/pytest-dev/pytest/issues/935
+# This forces an order to plugins of a sort
+codes_msg 'installing pytest plugins to force import order'
+for f in pytest-xdist pytest-forked; do
+    pip uninstall -y "$f" >& /dev/null || true
+    pip install -y "$f"
+done
