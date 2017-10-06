ec4cd3ae4b9c623571bf37f4f1dcdfaec94cb654
robnagler <github@q33.us>
2017-10-06 19:09:50 +0000

    try a new order

--- installers/code/codes/pykern.sh
+++ installers/code/codes/pykern.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 codes_download pykern
+# https://github.com/pytest-dev/pytest/issues/935
+pip uninstall -y pykern >& /dev/null || true
+for f in pytest-xdist pytest-forked pykern; do
+    pip uninstall -y "$f" >& /dev/null || true
+done
 pip install -r requirements.txt
 python setup.py install
 pyenv rehash
 hash pykern
-# https://github.com/pytest-dev/pytest/issues/935
-# This forces an order to plugins of a sort
-codes_msg 'installing pytest plugins to force import order'
-for f in pytest-xdist pytest-forked; do
-    pip uninstall -y "$f" >& /dev/null || true
-done
-# Only install when the test runs
