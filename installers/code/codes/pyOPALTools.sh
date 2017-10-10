f2e5ccedee34b45563993397651931eda9b7e564
robnagler <github@q33.us>
2017-10-10 22:55:07 +0000

    final release of opal-1.6

--- installers/code/codes/pyOPALTools.sh
+++ installers/code/codes/pyOPALTools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # https://gitlab.psi.ch/OPAL/pyOPALTools 500MB with data set
-codes_download_foss pyOPALTools-20170805.204337.tar.gz
+codes_download_foss pyOPALTools-20171010.092318.tar.gz
 python setup.py install
