3d86bc120f98d26864a9da138d018650c409267b
robnagler <github@q33.us>
2017-11-30 14:36:24 +0000

    containers build simplified

--- installers/container-build/radiasoft-download.sh
+++ installers/container-build/radiasoft-download.sh
@@ -2,16 +2,4 @@
 #
 # To run: curl radia.run | bash -s container-build
 #
-code_main() {
-    local s=../containers/bin/build
-    if [[ ! -x $s ]]; then
-        local prev_d=$(pwd)
-        install_tmp_dir
-        git clone -b "$install_github_channel" -q https://github.com/radiasoft/containers
-        s=$(pwd)/containers/$s
-        cd "$prev_d"
-    fi
-    "$s" "${install_extra_args[@]}"
-}
-
-code_main
+install_repo radiasoft/containers
