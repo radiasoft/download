fd4a8e5c0954a68b9924f821d3f3ceaf59c09471
robnagler <github@q33.us>
2017-12-05 16:51:05 +0000

    can't seem to get opal to compile

--- installers/code/codes/opal.sh
+++ installers/code/codes/opal.sh
@@ -17,6 +17,11 @@ CC=mpicc CXX=mpicxx ./configure \
 make install
 cd "$base_pwd"
 
+# /home/vagrant/src/radiasoft/codes/opal-20171212.221025/OPAL-1.9-20171123.105913/opt-pilot/Expression/Parser/expression_def.hpp:178:50:   required from ‘client::parser::expression<Iterator>::expression(client::error_handler<Iterator>&) [with Iterator = __gnu_cxx::__normal_iterator<const char*, std::__cxx11::basic_string<char> >]’
+# /home/vagrant/src/radiasoft/codes/opal-20171212.221025/OPAL-1.9-20171123.105913/opt-pilot/Expression/Parser/expression.cpp:10:33:   required from here
+# /usr/include/boost/phoenix/function/detail/cpp03/preprocessed/function_operator_10.hpp:57:# 76: error: invalid conversion from ‘const char*’ to ‘param_type {aka char*}’ [-fpermissive]
+#              return detail::expression::function_eval<F, A0 , A1 , A2>::make(f, a0 , a1 , a2);
+
 #codes_download https://gitlab.psi.ch/OPAL/src.git OPAL-1.9
 # The git repo is 1.6G, and takes a long time to load. The tgz is 3M
 # Last known working version of OPAL
