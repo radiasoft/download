43f35f2e71ba57704d3aa4effad0fdf0ec492306
robnagler <github@q33.us>
2018-02-07 12:38:21 +0000

    fedora is hard constraint

--- installers/code/codes.sh
+++ installers/code/codes.sh
@@ -215,7 +215,7 @@ codes_yum() {
 if [[ $0 == ${BASH_SOURCE[0]} ]]; then
     v=( $(cat /etc/fedora-release 2> /dev/null || true) )
     if (( ${v[2]:-0} < 21 )); then
-        codes_msg 'Only Fedora 21 or greater is supported at this time'
+        codes_err 'Only Fedora 21 or greater is supported at this time'
     fi
     unset v
     # make sure pyenv loaded
