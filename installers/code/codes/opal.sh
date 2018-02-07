3e6b260058e9fa019e5c14f5c4cb5bd91718ca12
robnagler <github@q33.us>
2018-02-07 16:50:27 +0000

    upgrade to H5hut-2.0.0rc3.tar.gz

--- installers/code/codes/opal.sh
+++ installers/code/codes/opal.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 base_pwd=$PWD
 # original source has bad ssl cert:
-# https://amas.psi.ch/H5hut/raw-attachment/wiki/DownloadSources/H5hut-1.99.13.tar.gz
-# Doc: https://amas.psi.ch/H5hut/wiki/H5hutInstall
-codes_download_foss H5hut-1.99.13.tar.gz
-patch -p0 < "$codes_data_src_dir"/opal/H5hut-1.99.13.patch
+# http://amas.web.psi.ch/Downloads/H5hut/H5hut-2.0.0rc3.tar.gz
+# Doc: https://gitlab.psi.ch/H5hut/src/wikis/home
+codes_download_foss H5hut-2.0.0rc3.tar.gz
+#patch -p0 < "$codes_data_src_dir"/opal/H5hut-1.99.13.patch
 perl -pi -e 's{\`which}{\`type -p}' autogen.sh
 ./autogen.sh
 perl -pi -e 's{\`which}{\`type -p}' configure
@@ -13,8 +13,7 @@ CC=mpicc CXX=mpicxx ./configure \
   --prefix="$(pyenv prefix)" \
   --with-pic \
   --enable-shared
-# Cannot run in parallel, bad dependencies
-make install
+codes_make_install
 cd "$base_pwd"
 
 # /home/vagrant/src/radiasoft/codes/opal-20171212.221025/OPAL-1.9-20171123.105913/opt-pilot/Expression/Parser/expression_def.hpp:178:50:   required from ‘client::parser::expression<Iterator>::expression(client::error_handler<Iterator>&) [with Iterator = __gnu_cxx::__normal_iterator<const char*, std::__cxx11::basic_string<char> >]’
