6f26c499d1374226c36201d4f08acbdcfba0e71b
robnagler <github@q33.us>
2018-02-12 18:22:29 +0000

    jspec needs manual install

--- installers/code/codes/jspec.sh
+++ installers/code/codes/jspec.sh
@@ -4,4 +4,5 @@ codes_download zhanghe9704/electroncooling master
 mkdir build
 cd build
 CMAKE_PREFIX_PATH="$(pyenv prefix)" cmake ..
-codes_make_install
+codes_make_install all
+install -m 755 jspec "$(pyenv prefix)"/bin/jspec
