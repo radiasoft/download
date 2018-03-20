c8d8f6ad591b5e4116edc9f7bf0a1768e6e8c4f4
robnagler <github@q33.us>
2018-03-20 17:38:15 +0000

    force a new timestamp on install.sh

--- bin/index.sh
+++ bin/index.sh
@@ -7,4 +7,4 @@ _u=https://raw.githubusercontent.com/radiasoft/download/master
 if [[ -n $install_server && $install_server != github ]]; then
     _u=$install_server/radiasoft/download
 fi
-curl -s -S -L "$_u/bin/install.sh" | bash -s "$@"
+curl -s -S -L "$_u/bin/install.sh?$(date +%s)" | bash -s "$@"
