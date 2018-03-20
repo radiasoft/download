89e76d6a2e36911677265fe657465e586bf72a6e
robnagler <github@q33.us>
2018-03-20 17:47:46 +0000

    fpc

--- installers/perl-dev/radiasoft-download.sh
+++ installers/perl-dev/radiasoft-download.sh
@@ -3,8 +3,8 @@
 # To run: curl radia.run | bash -s perl-dev
 #
 perl_dev_main() {
-    if [[ ! -r /etc/redhat-release ]]; then
-        install_err 'only works on Red Hat flavored Linux'
+    if ! fgrep -s -q ' release 7.' /etc/redhat-release; then
+        install_err 'only works on CentOS/7 (RHEL/7) Linux'
     fi
     if (( $EUID == 0 )); then
         install_err 'run as vagrant (or other ordinary user), not root'
@@ -14,7 +14,7 @@ export BIVIO_WANT_PERL=1
 export BIVIO_HTTPD_PORT=8000
 EOF
     install_repo_as_root biviosoftware/container-perl
-    set +e
+    set +euo pipefail
     . ~/.bashrc
     bivio sql init_dbms >& /dev/null || true
     # always recreate db
