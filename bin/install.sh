2698b010870097d9413b4e26cfd6d3945c6e4fd2
robnagler <github@q33.us>
2018-03-20 17:36:51 +0000

    fpc: index.html > index.sh

--- bin/install.sh
+++ bin/install.sh
@@ -368,7 +368,7 @@ install_repo() {
 install_repo_as_root() {
     (
         install_url radiasoft/download bin
-        install_download index.html
+        install_download index.sh
     ) | install_sudo "install_server=$install_server" "install_channel=$install_channel" "install_debug=$install_debug" bash -s "$@"
 }
 
