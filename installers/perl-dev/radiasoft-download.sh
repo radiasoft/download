3c61e3fbfc9cb5829f3abe38ca4f6e77f9138b29
robnagler <github@q33.us>
2018-03-30 22:46:09 +0000

    install bivio-perl as an rpm

--- installers/perl-dev/radiasoft-download.sh
+++ installers/perl-dev/radiasoft-download.sh
@@ -16,7 +16,12 @@ export BIVIO_HTTPD_PORT=8000
 EOF
     fi
     if ! perl -MGMP::Mpf -e 1 2>&1; then
-        install_repo_as_root biviosoftware/container-perl
+        install_repo_as_root biviosoftware/container-perl base
+        (
+            install_tmp_dir
+            install_download bivio-perl.rpm
+            install_yum_install bivio-perl.rpm
+        )
     fi
     if [[ $(psql 2>&1) =~ could.not.connect ]]; then
         sudo su - <<'EOF'
