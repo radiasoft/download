8b9ce8db30559240e0ae61680b1823cdaf4ceb4a
robnagler <github@q33.us>
2018-05-19 22:04:12 +0000

    use beamsim_build branch

--- installers/code/codes/rslinac.sh
+++ installers/code/codes/rslinac.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 codes_yum install boost-devel
-# Working commit on integration branch
-codes_download radiasoft/rslinac b0c58164f8405a976a06a5f89e27808abc474307
+codes_download radiasoft/rslinac beamsim_build
 python setup.py install
