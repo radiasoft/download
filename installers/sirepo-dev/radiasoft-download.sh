7e67641e4b33fdfbce7463e042d3bf32566889d0
robnagler <github@q33.us>
2018-07-01 21:43:23 +0000

    added sirepo_dev_docker install docker

--- installers/sirepo-dev/radiasoft-download.sh
+++ installers/sirepo-dev/radiasoft-download.sh
@@ -3,6 +3,39 @@
 # To run: curl radia.run | bash -s sirepo-dev
 #
 #TODO(robnagler) make sure git pull works on ~/src/biviosoftware/home-env pykern sirepo
+
+sirepo_dev_docker() {
+    if [[ -f /var/lib/docker ]]; then
+        return
+    fi
+    install_sudo bash <<'EOF'
+    set -euo pipefail
+    dnf -y -q install dnf-plugins-core
+    dnf -q config-manager \
+        --add-repo \
+        https://download.docker.com/linux/fedora/docker-ce.repo
+    dnf -q -y install docker-ce
+    usermod -aG docker vagrant
+    install -d -m 700 /etc/docker
+    mkfs.xfs -f -n ftype=1 /dev/mapper/docker-vps
+    mkdir /var/lib/docker
+    echo '/dev/mapper/docker-vps /var/lib/docker xfs defaults 0 0' >> /etc/fstab
+    mount /var/lib/docker
+    install -m 400 /dev/stdin /etc/docker/daemon.json <<'EOF2'
+{
+    "iptables": true,
+    "live-restore": true,
+    "storage-driver": "overlay2",
+    "storage-opts": [
+        "overlay2.override_kernel_check=true"
+    ]
+}
+EOF2
+    systemctl start docker
+    systemctl enable docker
+EOF
+}
+
 sirepo_dev_main() {
     if [[ ! -r /etc/redhat-release ]]; then
         install_err 'only works on Red Hat flavored Linux'
@@ -22,6 +55,7 @@ sirepo_dev_main() {
     if ! rpm -q SDDSPython >& /dev/null; then
         install_repo_eval code common
     fi
+    sirepo_dev_docker
     if ! type elegant >& /dev/null; then
         install_repo_eval code elegant
     fi
