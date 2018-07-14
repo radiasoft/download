b9a959e533d6ff43681ab59449f35b2116105c7c
robnagler <github@q33.us>
2018-07-14 00:52:08 +0000

    moved from sirepo-dev

---
+++ installers/redhat-docker/radiasoft-download.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# To run: curl radia.run | bash -s redhat_docker
+#
+set -euo pipefail
+
+redhat_docker_main() {
+    local data=/var/lib/docker
+    if [[ -f $data ]]; then
+        install_info "$data exists, so assuming docker installed"
+        return
+    fi
+    if selinuxenabled; then
+        perl -pi -e 's{(?<=^SELINUX=).*}{disabled}' /etc/selinux/config
+        install_err 'Disabled selinux. You need to "vagrant reload"'
+    fi
+    local vg=docker
+    # vps is supposed to be created by vagrant-persistent-storage
+    # but isn't on Fedora 27.
+    local lv=vps
+    local mdev=/dev/mapper/$vg-$lv
+    if [[ ! -e $mdev ]]; then
+        local bdev=/dev/sdb
+        if ! install_sudo fdisk -l "$bdev" >& /dev/null; then
+            install_info "$mdev does not exist, cannot install docker"
+            return
+        fi
+        if install_sudo fdisk -l "$bdev" | grep ^/dev >& /dev/null; then
+            install_info "$bdev contains mounted partisions, cannot install docker"
+            return
+        fi
+        install_sudo bash <<"EOF"
+        set -euo pipefail
+        pvcreate '$bdev'
+        vgcreate '$vg' '$bdev'
+        lvcreate -l 100%VG -n '$lv' '$vg'
+EOF
+    fi
+    install_sudo bash <<"EOF"
+    set -euo pipefail
+    if type dnf >& /dev/null; then
+        dnf -y -q install dnf-plugins-core
+        dnf -q config-manager \
+            --add-repo \
+            https://download.docker.com/linux/fedora/docker-ce.repo
+        dnf -y -q install docker-ce
+    else
+        yum-config-manager \
+            --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+        yum makecache fast
+        yum -y -q install yum-plugin-ovl
+        yum -y -q install docker-ce
+    fi
+    usermod -aG docker vagrant
+    install -d -m 700 /etc/docker
+    mkfs.xfs -f -n ftype=1 '$mdev'
+    mkdir '$data'
+    echo '$mdev $data xfs defaults 0 0' >> /etc/fstab
+    mount '$data'
+    install -m 400 /dev/stdin /etc/docker/daemon.json <<'EOF2'
+{
+    "iptables": true,
+    "live-restore": true,
+    "storage-driver": "overlay2",
+    "storage-opts": [
+        "overlay2.override_kernel_check=true"
+    ]
+}
+EOF2
+    systemctl start docker
+    systemctl enable docker
+EOF
+}
+
+redhat_docker_main "${install_extra_args[@]}"
