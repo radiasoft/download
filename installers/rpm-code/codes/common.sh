1889d0e84800776a50294824a0cbdcc5ae81a8f8
robnagler <github@q33.us>
2018-08-27 20:01:56 +0000

    share and man need to be owned by common

--- installers/rpm-code/codes/common.sh
+++ installers/rpm-code/codes/common.sh
@@ -11,3 +11,6 @@ pip install tables==3.3.0
 # Slows down builds of pykern, but doesn't affect development.
 codes_download pykern
 codes_python_install
+_common_pp=$(pyenv prefix)
+# otherwise directories are owned by root
+rpm_code_build_include_add "$_common_pp/share" "$_common_pp/man"
