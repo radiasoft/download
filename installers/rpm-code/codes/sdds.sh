57e7e76e6b86b280cec5e899a773681ccd1f752d
robnagler <github@q33.us>
2018-08-29 17:07:01 +0000

    need SDDSToolKit file list added

--- installers/rpm-code/codes/sdds.sh
+++ installers/rpm-code/codes/sdds.sh
@@ -6,3 +6,4 @@ rpm_code_build_install_files+=( $(rpm -ql SDDSToolKit) )
 cd "$sdds_pwd"
 codes_download_foss SDDSPython-3.2-1.fedora.27.x86_64.rpm
 install -m 0644 $(rpm -ql SDDSPython | grep ^/usr/lib/python2.7/site-packages) "$codes_pylib_dir"
+rpm -ql SDDSToolKit | fgrep -v /usr/lib/.build-id | rpm_code_build_include_add
