0c4876b328c0ed28abf591853cfdebd9cb589dc7
robnagler <github@q33.us>
2018-10-01 20:40:08 +0000

    no docker disk

--- installers/vagrant-rsconf-dev/radiasoft-download.sh
+++ installers/vagrant-rsconf-dev/radiasoft-download.sh
@@ -5,7 +5,7 @@
 set -euo pipefail
 vagrant_rsconf_dev_main() {
     if [[ ${1:-} != master ]]; then
-        export vagrant_dev_barebones=1 vagrant_dev_no_docker_disk=
+        export vagrant_dev_barebones=1
     fi
     install_repo_eval vagrant-centos7
 }
