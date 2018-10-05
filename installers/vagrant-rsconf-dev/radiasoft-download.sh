a791578d22b44383d2fa991ab619fdf82c5e29ea
robnagler <github@q33.us>
2018-10-05 16:53:18 +0000

    not tested

--- installers/vagrant-rsconf-dev/radiasoft-download.sh
+++ installers/vagrant-rsconf-dev/radiasoft-download.sh
@@ -5,24 +5,60 @@
 set -euo pipefail
 
 vagrant_rsconf_dev_main() {
-    if [[ ${1:-} != master ]]; then
-        export vagrant_dev_barebones=1 install_server=http://v3.radia.run:2916 install_channel=dev
-    fi
-    install_repo_eval vagrant-centos7
     if [[ ${1:-} == master ]]; then
-        return
+        install_server=file:///home/vagrant/src/radiasoft/rsconf/run/srv \
+            vagrant_rsconf_dev_master
+    else
+        vagrant_dev_barebones=1 \
+            install_server=http://v3.radia.run:2916 \
+            vagrant_rsconf_dev_worker
     fi
-    bivio_vagrant_ssh sudo su - <<EOF
-export install_channel=dev install_server="$install_server"
-# fails because of reboot
-curl "$install_server" | bash -s rsconf.sh "\$(hostname -f)" setup_dev || true
+}
+
+vagrant_rsconf_dev_master() {
+    install_repo_eval vagrant-centos7
+    bivio_vagrant_ssh <<'EOF'
+        bivio_pyenv_2
+        set -euo pipefail
+        sudo yum install -y nginx
+        mkdir -p ~/src/radiasoft
+        cd ~/src/radiasoft
+        gcl download
+        gcl containers
+        gcl pykern
+        cd pykern
+        pip install -e .
+        cd ..
+        gcl rsconf
+        cd rsconf
+        pip install -e .
+        mkdir -p rpm
+        cd rpm
+        curl -S -s -L -O https://depot.radiasoft.org/foss/bivio-perl-dev.rpm
+        curl -S -s -L -O https://depot.radiasoft.org/foss/perl-Bivio-dev.rpm
+        cd ..
+        rsconf build
 EOF
+    vagrant_rsconf_dev_run || true
     vagrant reload
+    vagrant_rsconf_dev_run
+    # For building perl rpms (see build-perl-rpm.sh)
+    bivio_vagrant_ssh sudo usermod -aG docker vagrant
+}
+
+vagrant_rsconf_dev_run() {
     bivio_vagrant_ssh sudo su - <<EOF
-set -e -x
-export install_channel=$install_channel install_server=$install_server
-curl "$install_server" | bash -s rsconf.sh "\$(hostname -f)" setup_dev
+        set -euo pipefail
+        export install_channel=dev install_server=$install_server
+        curl "$install_server/index.html" | bash -s rsconf.sh "\$(hostname -f)" setup_dev
 EOF
+}
+
+vagrant_rsconf_dev_worker() {
+    install_repo_eval vagrant-centos7
+    vagrant_rsconf_dev_run || true
+    vagrant reload
+    vagrant_rsconf_dev_run
 }
 
 vagrant_rsconf_dev_main ${install_extra_args[@]+"${install_extra_args[@]}"}
