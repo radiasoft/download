2c5743290b259f657b040a6fe3b7d573c86863e4
robnagler <github@q33.us>
2018-11-02 20:02:10 +0000

    build directly from SRW

--- installers/rpm-code/codes/srw.sh
+++ installers/rpm-code/codes/srw.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 codes_dependencies bnlcrl
 codes_yum_dependencies fftw2-devel
-# ochubar/SRW is over 600MB so GitHub times out sometimes. This is a
-# stripped down copy
-codes_download SRW-light '' SRW
+codes_download SRW
 cores=$(codes_num_cores)
 perl -pi -e "s/-j8/-j$cores/" Makefile
 perl -pi -e "s/'fftw'/'sfftw'/" cpp/py/setup.py
