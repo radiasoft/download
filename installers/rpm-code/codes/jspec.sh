d08346927d17706cfd273f38786e7cb2fd500b84
robnagler <github@q33.us>
2019-02-20 16:11:45 +0000

    fix https://github.com/radiasoft/container-beamsim/issues/3
    updated to 25230771a3cc01cb5d354015bfedb28d6bd178be

--- installers/rpm-code/codes/jspec.sh
+++ installers/rpm-code/codes/jspec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 codes_dependencies common
 codes_yum_dependencies muParser-devel
-codes_download zhanghe9704/electroncooling 73c67070e74ee802cbed2eea720fa023a695163f
+codes_download zhanghe9704/electroncooling 25230771a3cc01cb5d354015bfedb28d6bd178be
 codes_cmake
 codes_make_install all
 install -m 755 jspec "$(pyenv prefix)"/bin/jspec
