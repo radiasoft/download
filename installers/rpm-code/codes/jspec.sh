2a5795d351a860840ccb87a02ad4d9bd9fead695
robnagler <github@q33.us>
2019-02-22 18:16:45 +0000

    fix radiasoft/container-beamsim#3 use radiasoft fork
    compile issues fixed and pull requested, but need updated version now

--- installers/rpm-code/codes/jspec.sh
+++ installers/rpm-code/codes/jspec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 codes_dependencies common
 codes_yum_dependencies muParser-devel
-codes_download zhanghe9704/electroncooling 25230771a3cc01cb5d354015bfedb28d6bd178be
+codes_download radiasoft/electroncooling
 codes_cmake
 codes_make_install all
 install -m 755 jspec "$(pyenv prefix)"/bin/jspec
