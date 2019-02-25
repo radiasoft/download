70e216e657ab88db4c0e47eeda269219e65c1a67
robnagler <github@q33.us>
2019-02-25 22:39:22 +0000

    added pymesh

---
+++ installers/rpm-code/codes/pymesh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+codes_dependencies common
+codes_yum_dependencies mpfr-devel gmp-devel
+codes_download https://github.com/PyMesh/PyMesh.git
+git submodule update --init
+# Runs out of memory on a VM if there are not enough cores
+NUM_CORES=1 python setup.py build
+python setup.py install
+# run tests outside build directory
+cd ..
+python -c "import pymesh; pymesh.test()"
