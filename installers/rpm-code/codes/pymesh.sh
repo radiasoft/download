323e02caf185c8b4e66e35d87db9945563cefd63
robnagler <github@q33.us>
2019-02-26 17:50:03 +0000

    Patch setup.py to work with pip See https://github.com/radiasoft/ops/issues/329#issuecomment-467505708

--- installers/rpm-code/codes/pymesh.sh
+++ installers/rpm-code/codes/pymesh.sh
@@ -3,9 +3,10 @@ codes_dependencies common
 codes_yum_dependencies mpfr-devel gmp-devel
 codes_download https://github.com/PyMesh/PyMesh.git
 git submodule update --init
-# Runs out of memory on a VM if there are not enough cores
-NUM_CORES=1 python setup.py build
-codes_python_install
+# fixes hardwired directories which breaks cmake
+# also adds -j to pymesh cmake build
+patch setup.py < setup.py.patch
+NUM_CORES=$(codes_num_cores) codes_python_install
 # run tests outside build directory
 cd ..
 python -c "import pymesh; pymesh.test()"
