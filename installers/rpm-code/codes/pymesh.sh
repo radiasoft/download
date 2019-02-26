8b6c34b1bbd3d70ffec4184aa7c1b3c30e7bc2a6
robnagler <github@q33.us>
2019-02-26 01:39:34 +0000

    use codes_python_install so not easy install

--- installers/rpm-code/codes/pymesh.sh
+++ installers/rpm-code/codes/pymesh.sh
@@ -5,7 +5,7 @@ codes_download https://github.com/PyMesh/PyMesh.git
 git submodule update --init
 # Runs out of memory on a VM if there are not enough cores
 NUM_CORES=1 python setup.py build
-python setup.py install
+codes_python_install
 # run tests outside build directory
 cd ..
 python -c "import pymesh; pymesh.test()"
