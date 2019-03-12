c62bebce1d19e6760974ee3a341d192ea8bc29b3
robnagler <github@q33.us>
2019-03-12 19:20:46 +0000

    trying setting CONFIG_SITE.local

--- installers/rpm-code/codes/epics.sh
+++ installers/rpm-code/codes/epics.sh
@@ -2,5 +2,9 @@
 codes_dependencies common
 codes_download https://epics.anl.gov/download/base/base-7.0.2.tar.gz
 d=$HOME/epics
-make -j$(codes_num_cores) INSTALL_LOCATION="$d" EPICS_HOST_ARCH=linux-x86_64
+cat > configure/CONFIG_SITE.local <<EOF
+INSTALL_LOCATION = $d
+EPICS_HOST_ARCH = linux-x86_64
+EOF
+make -j"$(codes_num_cores)"
 rpm_code_build_include_add "$d"
