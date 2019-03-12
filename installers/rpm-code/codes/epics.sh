a4912ed185af5ec50cc269f3038484f76cd52972
robnagler <github@q33.us>
2019-03-12 07:40:21 +0000

    use literal make, since codes_make_install is confusing

--- installers/rpm-code/codes/epics.sh
+++ installers/rpm-code/codes/epics.sh
@@ -2,5 +2,5 @@
 codes_dependencies common
 codes_download https://epics.anl.gov/download/base/base-7.0.2.tar.gz
 d=$HOME/epics
-codes_make INSTALL_LOCATION="$d" EPICS_HOST_ARCH=linux-x86_64
+make -j$(codes_num_cores) INSTALL_LOCATION="$d" EPICS_HOST_ARCH=linux-x86_64
 rpm_code_build_include_add "$d"
