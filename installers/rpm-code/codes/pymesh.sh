6888c5e09debf62f36e7e0e05bede4ac03ef1ac0
robnagler <github@q33.us>
2019-03-28 19:57:58 +0000

    Fix https://github.com/radiasoft/ops/issues/349 build from radiasoft/PyMesh

--- installers/rpm-code/codes/pymesh.sh
+++ installers/rpm-code/codes/pymesh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 codes_dependencies common
 codes_yum_dependencies mpfr-devel gmp-devel
-codes_download https://github.com/PyMesh/PyMesh.git
+codes_download https://github.com/radiasoft/PyMesh.git
 git submodule update --init
 NUM_CORES=$(codes_num_cores) codes_python_install
 # run tests outside build directory
