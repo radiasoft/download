977f75078387665b7077e0a08fd28f1cc51e0c65
robnagler <github@q33.us>
2019-04-05 15:49:00 +0000

    use install_vars_export

--- installers/vagrant-sirepo-dev/radiasoft-download.sh
+++ installers/vagrant-sirepo-dev/radiasoft-download.sh
@@ -2,7 +2,7 @@
 vagrant_sirepo_dev_main() {
     install_repo_eval vagrant-dev fedora "$@"
     vagrant ssh <<EOF
-export install_server='$install_server' install_channel='$install_channel' install_debug='$install_debug'
+$(install_vars_export)
 source ~/.bashrc
 radia_run sirepo-dev
 EOF
