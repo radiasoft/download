188361f1655c2e8d3545e9b560dfc675053ef459
robnagler <github@q33.us>
2019-05-09 21:33:56 +0000

    cgal is so large that the clone needs to be restricted

--- installers/rpm-code/codes/pymesh.sh
+++ installers/rpm-code/codes/pymesh.sh
@@ -2,8 +2,9 @@
 codes_dependencies common
 codes_yum_dependencies mpfr-devel gmp-devel
 codes_download_nonrecursive=1 codes_download https://github.com/radiasoft/PyMesh.git
-git submodule update --init --depth 50 third_party/geogram
-git submodule update --init $(find third_party/* -maxdepth 0 -type d | grep -v geogram)
+git submodule update --init --depth=50 third_party/geogram
+# cgal is very large so use --depth=5
+git submodule update --init --depth=5 $(find third_party/* -maxdepth 0 -type d | grep -v geogram)
 NUM_CORES=$(codes_num_cores) codes_python_install
 # run tests outside build directory
 cd ..
