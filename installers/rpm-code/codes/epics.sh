7ece54b5d163a9cf7415c328f13ae9f6c328186e
robnagler <github@q33.us>
2019-06-07 14:47:56 +0000

    epics installed statically

--- installers/rpm-code/codes/epics.sh
+++ installers/rpm-code/codes/epics.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-codes_dependencies common
-codes_download https://epics.anl.gov/download/base/base-7.0.2.tar.gz
-d=$HOME/epics
-cat > configure/CONFIG_SITE.local <<EOF
-INSTALL_LOCATION = $d
-EPICS_HOST_ARCH = linux-x86_64
-EOF
-make -j"$(codes_num_cores)"
-rpm_code_build_include_add "$d"
+
+epics_main() {
+    codes_dependencies common
+    codes_download https://epics.anl.gov/download/base/base-7.0.2.tar.gz
+    local arch=linux-x86_64
+    make -j"$(codes_num_cores)" \
+         EPICS_HOST_ARCH="$arch" \
+         HOME="$HOME" \
+         LINKER_USE_RPATH=NO \
+         SHARED_LIBRARIES=NO
+    cd bin/"$arch"
+    ls | egrep -v '^S99|\.pl$' | xargs -n 100 install -m 555 "${codes_dir[bin]}"
+}
