152dc447f651fd0ed74f26d695420d2f3b664096
robnagler <bitbucket@q33.us>
2019-06-07 14:49:21 +0000

    pyzgoubi doesn't support py3 SyntaxError setup.py, line 23 except IOError, exc:

--- installers/rpm-code/codes/zgoubi.sh
+++ installers/rpm-code/codes/zgoubi.sh
@@ -7,7 +7,7 @@ zgoubi_python_install() {
 zgoubi_main() {
     codes_dependencies common
     codes_download radiasoft/zgoubi
-    zgoubi_python_versions='2 3'
+    zgoubi_python_versions=2
     # Lots of warnings so disable
     perl -pi -e 's{-Wall}{-w}' CMakeLists.txt
     codes_cmake -DCMAKE_INSTALL_PREFIX:PATH="${codes_dir[prefix]}"
