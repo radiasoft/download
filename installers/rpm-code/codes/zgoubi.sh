1d5376e868a6aec5e5a33bdd844e379252987e7b
robnagler <bitbucket@q33.us>
2019-06-07 14:27:18 +0000

    need to specify python versions

--- installers/rpm-code/codes/zgoubi.sh
+++ installers/rpm-code/codes/zgoubi.sh
@@ -7,6 +7,7 @@ zgoubi_python_install() {
 zgoubi_main() {
     codes_dependencies common
     codes_download radiasoft/zgoubi
+    zgoubi_python_versions='2 3'
     # Lots of warnings so disable
     perl -pi -e 's{-Wall}{}' CMakeLists.txt
     codes_cmake -DCMAKE_INSTALL_PREFIX:PATH="${codes_dir[prefix]}"
