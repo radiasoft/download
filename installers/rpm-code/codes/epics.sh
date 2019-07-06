ec9c88c3209b2c1d6b7800e9e12c47d389697e93
robnagler <github@q33.us>
2019-07-06 00:37:02 +0000

    fix #68 epics compiled with RPATH directly in ~/.local/epics epics has some pretty generically named libraries so rather than putting them in ~/.local/lib (which is in LD_LIBRARY_PATH), epics offers an RPATH option to its make, which we used. The libraries are very large (400M) compiles. Probably need to remove .a files, which are unnecessary. Programs are installed in ~/.local/bin, but need to be ~/.local/epics/bin, just in case they are hardwired along EPICS_BASE.

--- installers/rpm-code/codes/epics.sh
+++ installers/rpm-code/codes/epics.sh
@@ -3,12 +3,17 @@
 epics_main() {
     codes_dependencies common
     codes_download https://epics.anl.gov/download/base/base-7.0.2.tar.gz
+    cd ..
+    mv base-7.0.2 "$HOME"/.local/epics
+    cd "$HOME"/.local/epics
     local arch=linux-x86_64
     make -j"$(codes_num_cores)" \
          EPICS_HOST_ARCH="$arch" \
-         HOME="$HOME" \
-         LINKER_USE_RPATH=NO \
-         SHARED_LIBRARIES=NO
+         LINKER_USE_RPATH=YES \
+         SHARED_LIBRARIES=YES
+    # leave bin because there are other files (.pl & .pm) that may be referenced
+    # Some of these are large, e.g. modules
+    rm -rf modules documentation html src test
     cd bin/"$arch"
-    ls | egrep -v '^S99|\.pl$' | xargs -I % install -m 555 % "${codes_dir[bin]}"
+    ls | egrep -v '^S99|\.p[lm]$' | xargs -I % install -m 555 % "${codes_dir[bin]}"
 }
