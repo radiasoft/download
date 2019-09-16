b82da5c0452158c6c9fe98e662d137582aebefaa
robnagler <github@q33.us>
2019-09-16 21:43:01 +0000

    skip top-level makefile
    top level makefile calls "make" not $(MAKE) so can't pass -j flag

--- installers/rpm-code/codes/srw.sh
+++ installers/rpm-code/codes/srw.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 srw_python_install() {
-    cd SRW
-    make pylib
+    cd SRW/cpp/py
+    make python
     codes_python_lib_copy env/work/srw_python/{{srwl,uti}*.py,srwlpy*.so}
     find . -name srwlpy\*.so -exec rm {} \;
 }
@@ -14,9 +14,8 @@ srw_main() {
     codes_download ochubar/SRW
     # committed *.so files are not so good.
     find . -name \*.so -o -name \*.a -o -name \*.pyd -exec rm {} \;
-    cores=$(codes_num_cores)
-    perl -pi -e "s/-j\\s*8/-j$cores/" Makefile
     perl -pi -e "s/'fftw'/'sfftw'/" cpp/py/setup.py
     perl -pi -e 's/-lfftw/-lsfftw/; s/\bcc\b/gcc/; s/\bc\+\+/g++/' cpp/gcc/Makefile
-    make core
+    cd cpp/gcc
+    make -j"$(codes_num_cores)" lib
 }
