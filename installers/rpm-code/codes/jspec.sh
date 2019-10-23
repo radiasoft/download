eac9259bd0300494b7401f07320a58ab6e381c11
Paul Moeller <moeller@radiasoft.net>
2019-10-23 16:35:44 +0000

    use zhanghe9704 repo again

--- installers/rpm-code/codes/jspec.sh
+++ installers/rpm-code/codes/jspec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 codes_dependencies common
 codes_yum_dependencies muParser-devel
-codes_download radiasoft/electroncooling
+codes_download zhanghe9704/electroncooling
 codes_cmake
 codes_make_install all
 install -m 755 jspec "${codes_dir[bin]}"/jspec
