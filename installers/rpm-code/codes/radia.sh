fba105608ec304b01e858867239b86460216fef3
Paul Moeller <moeller@radiasoft.net>
2019-11-08 20:46:51 +0000

    use ochurbar repo for Radia

--- installers/rpm-code/codes/radia.sh
+++ installers/rpm-code/codes/radia.sh
@@ -11,7 +11,7 @@ radia_python_install() {
 radia_main() {
     # needed for fftw and uti_*.py
     codes_dependencies srw
-    codes_download robnagler/Radia feat/objgeometry
+    codes_download ochubar/Radia
     radia_python_versions='2 3'
     # committed *.so files are not so good.
     find . -name \*.so -o -name \*.a -o -name \*.pyd -exec rm {} \;
