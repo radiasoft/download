400a2d1ba277dc16164c588615d99a7c2f64cf43
robnagler <github@q33.us>
2019-11-30 21:36:49 +0000

    need to install_sudo

--- installers/slurm-dev/radiasoft-download.sh
+++ installers/slurm-dev/radiasoft-download.sh
@@ -21,11 +21,11 @@ radia_run slurm-dev
     slurm_dev_nfs
     install_yum install slurm-slurmd slurm-slurmctld
     dd if=/dev/urandom bs=1 count=1024 \
-        | install -m 400 -o munge -g munge /dev/stdin /etc/munge/munge.key
+        | install_sudo install -m 400 -o munge -g munge /dev/stdin /etc/munge/munge.key
     local f
     for f in munge slurmctld slurmd; do
-        systemctl start "$f"
-        systemctl enable "$f"
+        install_sudo systemctl start "$f"
+        install_sudo systemctl enable "$f"
     done
 
 }
