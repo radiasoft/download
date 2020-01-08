3379b305a30d0d53c89d6ee5ce05eaee385b6e71
robnagler <github@q33.us>
2020-01-08 22:23:36 +0000

    fix https://github.com/radiasoft/sirepo/issues/2154
    nersc_sirepo_update runs shifterimg pull, extracts commits,
    and installs in local pyenv

---
+++ installers/nersc-sirepo-update/radiasoft-download.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Update shifter image and pyenv
+#
+nersc_sirepo_update() {
+    local c=${1:-}
+    if [[ ! $c =~ ^(alpha|beta|prod)$ ]]; then
+        install_err "invalid channel=${c:-<missing arg>}"
+    fi
+    install_not_strict_cmd eval "$(pyenv init -)"
+    install_not_strict_cmd eval "$(pyenv virtualenv-init -)"
+    local i=docker:radiasoft/sirepo:$c
+    shifterimg pull "$i"
+    local v=sirepo-$c
+    if [[ ! -e ~/.pyenv/versions/$v ]]; then
+        pyenv virtualenv 3.7.2 "$v"
+    fi
+    pyenv shell "$v"
+    local p x
+    local d=~/"$v"/radiasoft
+    mkdir -p "$d"
+    cd "$d"
+    for p in pykern sirepo; do
+        if [[ ! -d "$p" ]]; then
+            git clone https://github.com/radiasoft/"$p"
+            cd "$p"
+        else
+            cd "$p"
+            git checkout master
+            git pull
+        fi
+        x=$(shifter --image="$i" /bin/bash -c "grep git-commit /home/vagrant/.pyenv/versions/py3/lib/python3*/site-packages/$p-20*.dist-info/METADATA")
+        if [[ ! $x =~ git-commit=(.*) ]]; then
+            install_err "missing git-commit for $p: output=$x"
+        fi
+        git checkout "${BASH_REMATCH[1]}"
+        pip uninstall -y . >& /dev/null || true
+        pip install .
+        cd ..
+    done
+    chmod 711 ~
+    chmod -R a+rX ~/.pyenv
+}
