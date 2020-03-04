baa229adb66d5aa97377810711d49517fb7f7852
robnagler <github@q33.us>
2020-03-04 20:40:15 +0000

    builds libgfortran.so.4

--- installers/rpm-code/codes/libgfortran4.sh
+++ installers/rpm-code/codes/libgfortran4.sh
@@ -3,9 +3,13 @@ libgfortran4_main() {
     codes_dependencies common
     codes_yum_dependencies libmpc-devel
     codes_download https://bigsearcher.com/mirrors/gcc/releases/gcc-7.5.0/gcc-7.5.0.tar.xz
-    mkdir ../gcc-build
-    cd ../gcc-build
-    ../gcc-7.5.0/configure --enable-languages=fortran --enable-checking=release --prefix=$HOME/.local --disable-multilib --disable-bootstrap
-    install -m 555 x86_64-pc-linux-gnu/libgfortran/.libs/libgfortran.so.4.0.0 \
-            "${codes_dir[lib]}"/libgfortran.so.4
+    cd ..
+    local p=$PWD/gcc-install
+    mkdir "$p"
+    mkdir gcc-build
+    cd gcc-build
+    ../gcc-7.5.0/configure --enable-languages=fortran --enable-checking=release --prefix="$p" --disable-multilib --disable-bootstrap
+    codes_make_install all
+    codes_make_install
+    install -m 555 "$p"/lib64/libgfortran.so.4.0.0 "${codes_dir[lib]}"/libgfortran.so.4
 }
