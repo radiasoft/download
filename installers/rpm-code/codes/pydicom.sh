ad2f7f2d654b4f8e9ecf8948e733cf3c0d25148e
robnagler <github@q33.us>
2020-03-25 02:05:15 +0000

    py3 only

--- installers/rpm-code/codes/pydicom.sh
+++ installers/rpm-code/codes/pydicom.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-codes_dependencies common
-pip install dicompyler-core==0.5.4 pydicom==1.0.2
+pydicom_python_install() {
+    pip install dicompyler-core==0.5.4 pydicom==1.0.2
+}
+
+pydicom_main() {
+    codes_dependencies common
+    pydicom_python_versions=3
+}
