dd1dd9d046c2e2d9a34659272176a6d859f8674a
robnagler <github@q33.us>
2020-05-03 20:18:39 +0000

    fix radiasoft/jupyter.radiasoft.org#79 downgrade 2.11.0

--- installers/rpm-code/codes/hypre.sh
+++ installers/rpm-code/codes/hypre.sh
@@ -2,7 +2,7 @@
 
 hypre_main() {
     codes_dependencies common
-    codes_download_foss hypre-2.11.2.tar.gz
+    codes_download_foss hypre-2.11.0.tar.gz
     cd src
     ./configure --prefix="${codes_dir[prefix]}"
     # hypre install does a chmod -R on install dirs, which
