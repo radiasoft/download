63a1f82d1ca6ea5d1bd81bfcdd508889cdd695c1
robnagler <github@q33.us>
2020-05-20 13:41:54 +0000

    simplify opencoarrays to not specify compilers

--- installers/rpm-code/codes/opencoarrays.sh
+++ installers/rpm-code/codes/opencoarrays.sh
@@ -3,7 +3,6 @@
 opencoarrays_main() {
     codes_dependencies common
     codes_download sourceryinstitute/OpenCoarrays
-    CC=gcc FC=gfortran \
-        codes_cmake -DCMAKE_INSTALL_PREFIX="${codes_dir[prefix]}"
+    codes_cmake -DCMAKE_INSTALL_PREFIX="${codes_dir[prefix]}"
     codes_make install
 }
