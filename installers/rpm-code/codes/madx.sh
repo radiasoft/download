991d2ebc4d7f35dbda405d26b9c925276b4313d8
Evan Carlin <evan@carlin.com>
2020-05-22 14:45:41 -0600

    cd .. unnecessary

--- installers/rpm-code/codes/madx.sh
+++ installers/rpm-code/codes/madx.sh
@@ -4,7 +4,6 @@ madx_main() {
     codes_dependencies common
     codes_download https://github.com/MethodicalAcceleratorDesign/MAD-X/archive/5.05.01.tar.gz MAD-X-5.05.01 madx 5.05.01
     codes_cmake
-    cd ..
     codes_make
     install -m 755 build/madx64  "${codes_dir[bin]}"/madx
 }
