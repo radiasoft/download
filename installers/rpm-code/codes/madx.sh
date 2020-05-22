7a8a428474f486c09d3f5b21416358bd06819536
Evan Carlin <evan@carlin.com>
2020-05-22 14:52:33 -0600

    different binary location

--- installers/rpm-code/codes/madx.sh
+++ installers/rpm-code/codes/madx.sh
@@ -5,5 +5,5 @@ madx_main() {
     codes_download https://github.com/MethodicalAcceleratorDesign/MAD-X/archive/5.05.01.tar.gz MAD-X-5.05.01 madx 5.05.01
     codes_cmake
     codes_make
-    install -m 755 build/madx64  "${codes_dir[bin]}"/madx
+    install -m 755 src/madx  "${codes_dir[bin]}"/madx
 }
