9c831de8427b3bc37cc473989686254f77c64196
robnagler <github@q33.us>
2020-12-22 23:51:04 +0000

    fix #138 remove -Wp,-D_GLIBCXX_ASSERTIONS

--- installers/fedora-patches/radiasoft-download.sh
+++ installers/fedora-patches/radiasoft-download.sh
@@ -3,10 +3,10 @@
 fedora_patches_mpich() {
     if grep -s -q format-security /usr/lib64/mpich/bin/mpifort; then
         # mpif77 and mpif90 are symlinks
-        install_sudo perl -pi -w -e 's{-Werror=format-security}{}' /usr/lib64/mpich/bin/mpifort
+        install_sudo perl -pi -w -e 's{-Werror=format-security|-Wp,-D_GLIBCXX_ASSERTIONS}{}g' /usr/lib64/mpich/bin/mpifort
     fi
     if ! grep -s -q -- '-Wformat' /usr/lib64/mpich/bin/mpic{c,xx}; then
-        install_sudo perl -pi -w -e 's{(?=-Werror=format-security)}{-Wformat }' /usr/lib64/mpich/bin/mpic{c,xx}
+        install_sudo perl -pi -w -e 's{(?=-Werror=format-security)}{-Wformat };s{-Wp,-D_GLIBCXX_ASSERTIONS}{};' /usr/lib64/mpich/bin/mpic{c,xx}
     fi
 }
 
