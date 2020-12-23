05455548bc13d9b38c2cfe950de4bfde7d6df7d4
robnagler <github@q33.us>
2020-12-23 19:32:56 +0000

    patches for gcc10 changes
    time_t declaration needs extern
    -fallow-invalid-boz needed for ' integer(Double), parameter :: dNaN    = Z"7FF8000000000000"'

--- installers/rpm-code/codes/madx.sh
+++ installers/rpm-code/codes/madx.sh
@@ -3,6 +3,11 @@
 madx_main() {
     codes_dependencies common
     codes_download https://github.com/MethodicalAcceleratorDesign/MAD-X/archive/5.05.01.tar.gz MAD-X-5.05.01 madx 5.05.01
+    perl -pi -e '
+        s{.3-9...0-9...0-9.}{[0-9]+\\\\.[0-9]+\\\\.+[0-9]};
+        s{(?=\-funroll-loops)}{-fallow-invalid-boz };
+    ' cmake/compilers/setupGNU.cmake
+    perl -pi -e 's{(?=^time_t)}{extern }' src/mad_gvar.h
     codes_cmake
     codes_make
     install -m 755 src/madx  "${codes_dir[bin]}"/madx
