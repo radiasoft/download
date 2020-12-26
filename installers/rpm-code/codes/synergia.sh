9e9a66fe308f33fcf6c169c38ceadf1fbbbf6dfd
robnagler <github@q33.us>
2020-12-26 00:54:45 +0000

    quoting

--- installers/rpm-code/codes/synergia.sh
+++ installers/rpm-code/codes/synergia.sh
@@ -4,12 +4,12 @@ synergia_python_install() {
     cd synergia2
     perl -pi -e 's{(?<=find_package.Python3)}{ 3.7.2 EXACT REQUIRED}' CMakeLists.txt
     codes_cmake \
-        -DBOOST_ROOT=$HOME/.local \
-        -DCHEF_DIR="${codes_dir[pyenv_prefix]}/lib/chef/cmake" \
-        -DCMAKE_INSTALL_PREFIX="${codes_dir[pyenv_prefix]}" \
-        -DFFTW3_LIBRARY_DIRS=/usr/lib64/mpich/lib \
-        -DUSE_PYTHON_3=1 \
-        -DUSE_SIMPLE_TIMER=0
+        -D BOOST_ROOT="${codes_dir[prefix]}" \
+        -D CHEF_DIR="${codes_dir[pyenv_prefix]}/lib/chef/cmake" \
+        -D CMAKE_INSTALL_PREFIX="${codes_dir[pyenv_prefix]}" \
+        -D FFTW3_LIBRARY_DIRS=/usr/lib64/mpich/lib \
+        -D USE_PYTHON_3=1 \
+        -D USE_SIMPLE_TIMER=0
     codes_make_install
     # synergia has this in multiple CMakeLists.txt
     #   install(FILES
@@ -17,7 +17,7 @@ synergia_python_install() {
     #    "${CMAKE_CURRENT_BINARY_DIR}/version.py"
     #    DESTINATION lib/synergia)
     # easier to just move it to the right place at the end.
-    mv "${codes_dir[pyenv_prefix]}/lib/synergia" $(codes_python_lib_dir)
+    mv "${codes_dir[pyenv_prefix]}/lib/synergia" "$(codes_python_lib_dir)"
     cd ..
     cp src/synergia/bunch/tests/test_bunch.py ..
     cd ..
