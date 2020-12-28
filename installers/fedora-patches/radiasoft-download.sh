fbf5573b2323a2db35c0feee1ef0faf2453873ec
robnagler <github@q33.us>
2020-12-28 20:15:40 +0000

    redhat-hardened-cc1 needs to be passed else cmake goes into a long compile test loop (it expects mpicc to run and not fail) turn off -g so symbols don't get added automatically document readelf for GCC.command.line

--- installers/fedora-patches/radiasoft-download.sh
+++ installers/fedora-patches/radiasoft-download.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 fedora_patches_mpich() {
+    # don't eliminate this, because this puts cmake in an infinite
+    # loop trying to find mpicc when building opal. For some
+    # reason opal needs to set CC=mpicc and CXX=mpicxx
+    # s{-specs=/usr/lib/rpm/redhat/redhat-hardened-cc1}{}g;
+
+    # to read the recorded switches:
+    # readelf -p .GCC.command.line a.out
+
     install_sudo perl -pi -w -e '
     s{-Wp,-D_FORTIFY_SOURCE=2}{}g;
     s{-Wp,-D_GLIBCXX_ASSERTIONS}{}g;
     s{-fplugin=annobin}{}g;
     s{-fstack-clash-protection}{}g;
     s{-fstack-protector-strong}{}g;
+    # this does not turn on symbols (-g)
+    s{-g(?=record-gcc-switches)}{-f}g;
     s{-fcf-protection}{}g;
-    s{-specs=/usr/lib/rpm/redhat/redhat-hardened-cc1}{}g;
     s{-specs=/usr/lib/rpm/redhat/redhat-annobin-cc1}{}g;
+    # these flags are supplied by make/cmake
+    s{-O2}{}g;
+    s{-g\b}{}g;
     ' /usr/lib64/mpich/bin/mpifort /usr/lib64/mpich/bin/mpic{c,xx}
     if grep -s -q format-security /usr/lib64/mpich/bin/mpifort; then
         # mpif77 and mpif90 are symlinks
