2c7e7bcb38d866109d419c10ef4c4db6083e6a60
robnagler <github@q33.us>
2021-01-05 15:10:44 +0000

    fix #137 slaclab/Genesis-1.3-Version2
    install symlinks for genesis2 and genesis2-mpi

--- installers/rpm-code/codes/genesis.sh
+++ installers/rpm-code/codes/genesis.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 genesis_main() {
     codes_dependencies common
-    codes_download_foss genesis-2.0-120629.tar.gz
-    # no parallel make
-    make
-    make multi
-    make EXECUTABLE=genesis_mpi COMPILER=mpif77
-    install -m 555 genesis genesis_mpi "${codes_dir[bin]}"
+    codes_download https://github.com/slaclab/Genesis-1.3-Version2.git
+    codes_cmake -D USE_MPI=OFF
+    codes_make all
+    install -m 555 genesis2 "${codes_dir[bin]}"/genesis.sh
+    cd ..
+    rm -rf build
+    codes_cmake -D USE_MPI=ON
+    codes_make all
+    install -m 555 genesis2-mpi "${codes_dir[bin]}"/genesis_mpi
+    ln -s genesis "${codes_dir[bin]}"/genesis2
+    ln -s genesis_mpi "${codes_dir[bin]}"/genesis2-mpi
 }
