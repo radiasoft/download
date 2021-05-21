81c73fd3b480665019b75758a630999222b69e43
git-user <git-email>
2021-05-21 21:00:57 +0000

    libgfortran4 is a code not pip install

--- installers/rpm-code/codes/shadow3.sh
+++ installers/rpm-code/codes/shadow3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 shadow3_main() {
-    codes_dependencies xraylib
+    codes_dependencies xraylib libgfortran4
 }
 
 shadow3_python_install() {
-    install_pip_install srxraylib shadow3 libgfortran4
+    install_pip_install srxraylib shadow3
     install_pip_install --no-deps OASYS1-ShadowOui SYNED
     local p=$(codes_python_lib_dir)/orangecontrib/shadow/util
     echo '# removed by RadiaSoft' > "$p"/__init__.py
