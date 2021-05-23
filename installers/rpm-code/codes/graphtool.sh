8e26f6a3b952217a060b2499990fc4eaff2fff3b
git-user <git-email>
2021-05-23 09:13:46 +0000

    seems to compile

--- installers/rpm-code/codes/graphtool.sh
+++ installers/rpm-code/codes/graphtool.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 graphtool_python_install() {
-    NUM_CORES=$(codes_num_cores) codes_python_install
     cd graph-tool-2.37
-./configure --enable-openmp --prefix=$(pyenv prefix) --with-boost=$HOME/.local --with-cgal=$HOME/.local --disable-cairo
-
-BOOSTLIBDIR=$HOME/.local/lib sh ./configure --enable-openmp --prefix=$(pyenv prefix) --with-boost=$HOME/.local --with-cgal=$HOME/.local --with-boost-python=boost_python37
-
-    ./configure --enable-openmp --prefix="${codes_dir[prefix]}" --docdir="/usr/share/doc/$pkgname"
-    codes_make
+    ./configure \
+        --enable-openmp \
+        --prefix="${codes_dir[pyenv_prefix]}" \
+        --with-boost="${codes_dir[prefix]}" \
+        --with-cgal="${codes_dir[prefix]}" \
+        --disable-cairo
+    codes_make_install
 }
 
 graphtool_main() {
-    local x=(
-        sparsehash-devel
-        graphviz
-    )
-    codes_yum_dependencies "${x[@]}"
-    codes_dependencies common
+    codes_yum_dependencies graphviz sparsehash-devel
+    codes_dependencies common cgal
     codes_download https://downloads.skewed.de/graph-tool/graph-tool-2.37.tar.bz2
 }
