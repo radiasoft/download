671af2bc95e5b4f25565ebf4d6ac47734ceb884f
git-user <git-email>
2021-05-23 09:13:29 +0000

    working

--- installers/rpm-code/codes/cgal.sh
+++ installers/rpm-code/codes/cgal.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 cgal_main() {
-    codes_dependencies common
-    codes_download https://github.com/CGAL/cgal/releases/download/v5.2.1/CGAL-5.2.1-library.tar.xz
+    codes_yum_dependencies gmp-devel mpfr-devel
+    codes_dependencies common boost
+    codes_download https://github.com/CGAL/cgal/releases/download/v5.2.1/CGAL-5.2.1-library.tar.xz CGAL-5.2.1
     codes_cmake_fix_lib_dir
-    codes_cmake -DCMAKE_INSTALL_PREFIX="${codes_dir[prefix]}"
+    codes_cmake \
+        -DBOOST_ROOT="${codes_dir[prefix]}" \
+        -DCMAKE_INSTALL_PREFIX="${codes_dir[prefix]}"
     codes_make install
 }
