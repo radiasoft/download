54b3df280e5e592d62cee2bb6c91d8cd301f5c1c
git-user <git-email>
2021-08-26 19:24:31 +0000

    add lost documentation back in

--- installers/rpm-code/codes/ipykernel.sh
+++ installers/rpm-code/codes/ipykernel.sh
@@ -5,6 +5,8 @@ ipykernel_main() {
     install_pip_install ipykernel
     local i=3
     local v=py$i
+    # http://ipython.readthedocs.io/en/stable/install/kernel_install.html
+    # http://www.alfredo.motta.name/create-isolated-jupyter-ipython-kernels-with-pyenv-and-virtualenv/
     local where=( $(python -m ipykernel install --display-name "Python $i" --name "$v" --user) )
     where=${where[-1]}
     if [[ ! $where  =~ ${codes_dir[share]} ]]; then
