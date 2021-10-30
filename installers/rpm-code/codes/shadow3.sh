cd954ad39239909f15d3f5a0ac391aa430d9cbc7
robnagler <github@q33.us>
2021-10-30 13:13:28 +0000

    share code

--- installers/rpm-code/codes/shadow3.sh
+++ installers/rpm-code/codes/shadow3.sh
@@ -7,13 +7,12 @@ shadow3_main() {
 shadow3_python_install() {
     install_pip_install srxraylib shadow3
     install_pip_install --no-deps OASYS1-ShadowOui SYNED OASYS1-ShadowOui-Advanced-Tools
-    local p=$(codes_python_lib_dir)/orangecontrib/shadow/util
-    echo '# removed by RadiaSoft' > "$p"/__init__.py
+    local p=$(codes_python_lib_dir)/orangecontrib
+    local u=$p/shadow/util
+    echo '# removed by RadiaSoft' > "$u"/__init__.py
     perl -pi -e '/SourceUndulatorFactory(Srw|Pysru)\s*$/ && ($_ = "")' \
-         "$p"/undulator/source_undulator.py
+         "$u"/undulator/source_undulator.py
     perl -pi -e '/from oasys.*$/ && ($_ = "")' \
-         "$p"/shadow_util.py
-    local p=$(codes_python_lib_dir)/orangecontrib/shadow_advanced_tools/util
-    perl -pi -e '/from oasys.*$/ && ($_ = "")' \
-         "$p"/fresnel_zone_plates/fresnel_zone_plate_simulator.py
+         "$u"/shadow_util.py \
+         "$p"/shadow_advanced_tools/util/fresnel_zone_plates/fresnel_zone_plate_simulator.py
 }
