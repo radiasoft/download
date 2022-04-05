fba7f4a1d31de43fde52d6d56b946b8d3052fb2e
Evan Carlin <evan@carlin.com>
2022-04-05 16:01:14 -0600

    Fix #203: installer for nersc development (#205)
    * Fix #203: installer for nersc development

---
+++ installers/nersc-pyenv/radiasoft-download.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# To run: curl radia.run | bash -s nersc-pyenv
+#
+nersc_dev_main() {
+    local r=~/.pyenv
+    if [[ ! -d $r ]]; then
+        curl -s -S -L https://pyenv.run | bash
+    fi
+    PATH="$r/bin:$PATH"
+    eval "$(pyenv init --path)"
+    if [[ ! -d $r/plugins/pyenv-virtualenv ]]; then
+        git clone https://github.com/pyenv/pyenv-virtualenv.git "$r"/plugins/pyenv-virtualenv
+    fi
+    eval "$(pyenv virtualenv-init -)"
+    local v='3.7.2'
+    if [[ ! -e $r/versions/$v ]]; then
+        PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install "$v"
+    fi
+    local a=py3
+    if [[ ! -e $r/versions/$a ]]; then
+        pyenv virtualenv "$v" "$a"
+    fi
+    pyenv global "$a"
+    if ! grep 'pyenv init' ~/.bashrc.ext; then
+        perl -pi -e '/exiting .bashrc.ext/ && ($_ = q{
+if ! [[ $PATH =~ pyenv/bin ]]; then
+    export PATH="~/.pyenv/bin:$PATH"
+    eval "$(pyenv init --path)"
+    eval "$(pyenv virtualenv-init -)"
+fi
+} . $_)' ~/.bashrc.ext
+    fi
+}
