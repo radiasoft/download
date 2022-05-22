9e5f21e8f82a3d939646a413a738d752d6117a7c
robnagler <github@q33.us>
2022-05-22 02:34:25 +0000

    chown back

--- installers/ci-pull-request/radiasoft-download.sh
+++ installers/ci-pull-request/radiasoft-download.sh
@@ -28,6 +28,8 @@ ci_pull_request_main() {
             ;;
     esac
     local d=$PWD
+    local u=$(stat -c %U .)
+    local g=$(stat -c %G .)
     set -x
     docker run -v "$d:$d" -i -u root --rm "$i:alpha" bash <<EOF | cat
         set -eou pipefail
@@ -46,6 +48,7 @@ ci_pull_request_main() {
             pip install -e .
             pykern test
 EOF2
+        chown -R '$u:$g' .
 EOF
     set +x
 }
