21e0a27fe2b82334512f59c1f3305a14db0b1628
robnagler <github@q33.us>
2022-06-23 22:58:38 +0000

    untested: rewrite beamsim-codes to allow sharing build all

--- installers/beamsim-codes/radiasoft-download.sh
+++ installers/beamsim-codes/radiasoft-download.sh
@@ -1,41 +1,143 @@
 #!/bin/bash
 
-beamsim_codes_main() {
+declare -a _beamsim_codes_all=(
+    common
+
+    # a simple one first
+    genesis
+
+    # libraries
+    pydot
+    boost
+    ml
+
+    rsbeams
+
+    # create a delay here so radiasoft.repo in radiasoft/rpm-code
+    # is "old" by the time bnlcrl (or other fast build) happens
+    # otherwise, the cache will be stale
+    fnal_chef
+    synergia
+
+    jspec
+
+    bnlcrl
+    # depends on ml
+    srw
+    radia
+
+    ipykernel
+    bluesky
+
+    elegant
+
+    epics
+
+    hypre
+
+    h5hut
+    parmetis
+    metis
+    trilinos
+# see pyopaltools.sh
+#    pyopaltools
+    opal
+
+    madx
+
+    openmc
+
+    # needs hypre and metis
+    petsc
+    slepc
+    petsc4py
+    slepc4py
+    fenics
+
+    pydicom
+
+    pymesh
+
+    forthon
+    openpmd
+    pygist
+    warp
+
+    libgfortran4
+    xraylib
+    shadow3
+
+    pyzgoubi
+    zgoubi
+
+)
+
+# Some of these are deps and others are just build deps.
+# If something is missed from this list, it will get installed,
+# which is probably no harm done. trilinos is the big one to not install.
+declare _beamsim_codes_intall_skip=(
+    pydot
+    boost
+    fnl_chef
+    h5hut
+    parmetis
+    metis
+    trilinos
+    petsc
+    slepc
+    petsc4py
+    slepc4py
+    forthon
+    openpmd
+    pygist
+    libgfortran4
+    xraylib
+    pyzgoubi
+)
+
+beamsim_codes_build() {
+    local script=$1
+    local start_at=${2:-}
+    for c in "${_beamsim_codes_all[@]}"; do
+        if [[ $start_at ]]; then
+            if [[ $start_at != $c ]]; then
+                continue
+            fi
+            start_at=
+        fi
+        echo "$c"
+        bash "$script" "$c"
+    done
+}
+
+beamsim_codes_install() {
     # Ensure everything is up to date first
     # If there are codes already installed, they'll update common,
     # etc. first, which may be required for later codes.
     install_yum update
-    local codes=(
-        # include common here even though a dependency so
-        # that the latest gets installed without bloating
-        # the container with an update (below). By default
-        # only the version that's required by the other packages
-        # gets installed.
-        common
-
-        bluesky
-        elegant
-        epics
-        fenics
-        genesis
-        ipykernel
-        jspec
-        hypre
-        madx
-        ml
-        opal
-        openmc
-        pydicom
-        pymesh
-        rsbeams
-        shadow3
-        srw
-        # depends on srw
-        radia
-        synergia
-        warp
-        zgoubi
-    )
-    install_repo_eval code "${codes[@]}"
+    # POSIT: codes do not have special or spaces
+    install_repo_eval code $(beamsim_codes_install_list)
     install_repo_eval fedora-patches
+}
+
+beamsim_codes_install_list() {
+    local IFS='|'
+    local r='^('"${_beamsim_codes_intall_skip[*]}"')$'
+    local c
+    for c in "${_beamsim_codes_all[@]}"; do
+        if [[ ! $c =~ $r ]]; then
+            echo "$c"
+        fi
+    done
+
+}
+
+beamsim_codes_main() {
+    local build=${1:-}
+    if [[ $build == build ]]; then
+        shift
+        beamsim_codes_build "$@"
+        return
+    fi
+    beamsim_codes_install
 }
