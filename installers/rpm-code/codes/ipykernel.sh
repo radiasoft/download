aaaeb0f213d3d8c7b4ec9eae87ea01dc32de3d4c
Evan Carlin <evan@carlin.com>
2022-07-04 17:16:32 +0000

    env will now come through c.Spawner.env_keep

--- installers/rpm-code/codes/ipykernel.sh
+++ installers/rpm-code/codes/ipykernel.sh
@@ -6,20 +6,13 @@ ipykernel_main() {
 }
 
 ipykernel_python_install() {
-    local -a e=()
-    for x in LD_LIBRARY_PATH PKG_CONFIG_PATH PYTHONPATH; do
-        if [[ ${!x:-} ]]; then
-            e+=" --env $x ${!x}"
-        fi
-    done
     # http://ipython.readthedocs.io/en/stable/install/kernel_install.html
     # http://www.alfredo.motta.name/create-isolated-jupyter-ipython-kernels-with-pyenv-and-virtualenv/
     local -a l=$(python -m ipykernel install \
         --display-name "Python $1" \
         --name "$2" \
         --user \
-        --env PYENV_VERSION "$2" \
-        ${e[@]})
+        --env PYENV_VERSION "$2")
     l=${l[-1]}
     if [[ ! $l =~ ${codes_dir[share]} ]]; then
         install_err "expecting $l to be a subdirectory of ${codes_dir[share]}"
