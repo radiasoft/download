42a8b408fbbd3bbfe3a34867351325f137844d2d
robnagler <github@q33.us>
2022-07-12 15:24:31 -0600

    fix #270 look for ~/src

--- installers/rpm-code/dev-env.sh
+++ installers/rpm-code/dev-env.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 # intended to be sourced
+cd "$(dirname "${BASH_SOURCE[0]}")"
+_root() {
+    local r=$PWD
+    while [[ $(basename $r) != src ]]; do
+        d=$(dirname "$r")
+        echo $d
+        if [[ $r == $d ]]; then
+            echo 'ERROR: could not find root: using ~/src' 1>&2
+            echo ~/src
+            return 1
+        fi
+        r=$d
+    done
+    echo $r
+}
 export dev_port=2916
 export install_server=http://$(hostname -f):$dev_port
 export fedora_version=32
-export repo_rel_dir=fedora/$fedora_version/$(arch)/dev
-export rpm_code_install_dir=$HOME/src/yum/$repo_rel_dir
-export radiasoft_repo_file=$(dirname $(dirname $rpm_code_install_dir))/radiasoft.repo
+export repo_fedora_dir=$(_root)/src/yum/fedora
+export rpm_code_install_dir=$repo_fedora_dir/$fedora_version/$(arch)/dev
+export radiasoft_repo_file=$repo_fedora_dir/radiasoft.repo
 export install_proprietary_key=proprietary_code
 # for convenience to test rpm-perl, not used here
-export rpm_perl_install_dir=$HOME/src/radiasoft/rsconf/rpm
-cd "$(dirname "${BASH_SOURCE[0]}")"
+export rpm_perl_install_dir=$r/src/radiasoft/rsconf/rpm
+unset _root
