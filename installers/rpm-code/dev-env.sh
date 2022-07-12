5fedd36d4cfbce6244ac3b672da1eecafec560ab
robnagler <github@q33.us>
2022-07-12 23:16:46 +0000

    Fix errors from review

--- installers/rpm-code/dev-env.sh
+++ installers/rpm-code/dev-env.sh
@@ -5,23 +5,22 @@ _root() {
     local r=$PWD
     while [[ $(basename $r) != src ]]; do
         d=$(dirname "$r")
-        echo $d
         if [[ $r == $d ]]; then
-            echo 'ERROR: could not find root: using ~/src' 1>&2
-            echo ~/src
+            echo "ERROR: could not find root: using $HOME/src" 1>&2
+            echo "$HOME/src"
             return 1
         fi
         r=$d
     done
-    echo $r
+    echo "$r"
 }
 export dev_port=2916
 export install_server=http://$(hostname -f):$dev_port
 export fedora_version=32
-export repo_fedora_dir=$(_root)/src/yum/fedora
+export repo_fedora_dir=$(_root)/yum/fedora
 export rpm_code_install_dir=$repo_fedora_dir/$fedora_version/$(arch)/dev
 export radiasoft_repo_file=$repo_fedora_dir/radiasoft.repo
 export install_proprietary_key=proprietary_code
 # for convenience to test rpm-perl, not used here
-export rpm_perl_install_dir=$r/src/radiasoft/rsconf/rpm
+export rpm_perl_install_dir=$(_root)/radiasoft/rsconf/rpm
 unset _root
