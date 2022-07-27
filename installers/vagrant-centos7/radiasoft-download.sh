8ea3239b8fb90340644c89538201498e10d176cc
Rob Nagler <github@q33.us>
2022-07-27 14:46:11 -0600

    Fix #303 centos7 installer removed, just use centos (#304)

--- installers/vagrant-centos7/radiasoft-download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Create a Centos 7 VirtualBox
-#
-# Usage: curl radia.run | bash -s vagrant-centos7 [guest-name:v.radia.run [guest-ip:10.10.10.10]]
-#
-install_repo_eval vagrant-dev centos/7 ${install_extra_args[@]+"${install_extra_args[@]}"}
