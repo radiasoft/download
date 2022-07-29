7e54ebf225c5d7a4745bd553e1ada305e8d88b6f
Evan Carlin <evan@carlin.com>
2022-07-29 16:55:58 +0000

    fix #313: vagrant ssh expects cmds with -c or STDIN

--- installers/vagrant-rsconf-dev/radiasoft-download.sh
+++ installers/vagrant-rsconf-dev/radiasoft-download.sh
@@ -46,7 +46,8 @@ EOF
 }
 
 vagrant_rsconf_dev_run() {
-    install_server=$install_server vagrant ssh sudo su - <<EOF
+    install_server=$install_server vagrant ssh <<EOF
+        sudo su -
         set -euo pipefail
         export install_channel=dev install_server=$install_server
         curl "$install_server/index.html" | bash -s rsconf.sh "\$(hostname -f)" setup_dev
