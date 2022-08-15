8dadfaf7dfe7ca2faf3eec1b969714cc16254915
robnagler <github@q33.us>
2022-08-15 23:28:10 +0000

    fnal_chef remove -Werror

--- installers/rpm-code/codes/fnal_chef.sh
+++ installers/rpm-code/codes/fnal_chef.sh
@@ -8,14 +8,10 @@ fnal_chef_main() {
 
 fnal_chef_python_install() {
     cd chef
--Wno-error=deprecated-declarations
-
-/home/vagrant/src/radiasoft/codes/fnal_chef-20220614.144145/chef/build/include/basic_toolkit/TVector.h:203:30: error: ‘template<class _Arg1, class _Arg2, class _Result> struct std::binary_function’ is deprecated [-Werror=deprecated-declarations]
-  203 |   class op_mult: public std::binary_function<std::complex<double>&, std::complex<double>&, std::complex<double> > {
-      |                              ^~~~~~~~~~~~~~~
--Werror=deprecated-declarations
-
-    perl -pi -e "s{(?<=find_package.Python3)}{ $RADIA_RUN_VERSION_PYTHON EXACT REQUIRED}" CMakeLists.txt
+    perl -pi -e "
+        s{-Werror}{};
+        s{(?<=find_package.Python3)}{ $RADIA_RUN_VERSION_PYTHON EXACT REQUIRED};
+    " CMakeLists.txt
     codes_cmake \
         -D BOOST_ROOT="${codes_dir[prefix]}" \
         -D CMAKE_INSTALL_PREFIX="${codes_dir[pyenv_prefix]}" \
