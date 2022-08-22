37076da48c8cd4f1182b7bcd515c9fab03bef36e
robnagler <github@q33.us>
2022-08-22 01:14:27 +0000

    ckp

--- installers/rpm-code/dev-env.sh
+++ installers/rpm-code/dev-env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # intended to be sourced
 cd "$(dirname "${BASH_SOURCE[0]}")"
+
 _root() {
     declare r=$PWD
     declare d
@@ -15,12 +16,13 @@ _root() {
     done
     echo "$r"
 }
+
 export dev_port=2916
-export install_version_fedora=${install_version_fedora:-36}
 export install_server=http://$(hostname -f):$dev_port
+export install_version_fedora=${install_version_fedora:-36}
 export rpm_code_install_dir=$(_root)/yum/fedora/$install_version_fedora/$(arch)/dev
 export radiasoft_repo_file=$rpm_code_install_dir/radiasoft.repo
 export install_proprietary_key=proprietary_code
 # for convenience to test rpm-perl, not used here
 export rpm_perl_install_dir=$(_root)/radiasoft/rsconf/rpm
-unset _root d
+unset _root
