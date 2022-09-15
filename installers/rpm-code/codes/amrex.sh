dbd265b6680b4eb84bc2a289b089c5f6489c92a2
garsuga <gsugarbaker@gmail.com>
2022-09-15 19:31:44 +0000

    create amrex installer

---
+++ installers/rpm-code/codes/amrex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+amrex_main() {
+    codes_dependencies common
+    codes_download https://github.com/AMReX-Codes/amrex/releases/download/22.09/amrex-22.09.tar.gz amrex
+    ./configure \
+        --prefix="${codes_dir[prefix]}"
+    codes_make_install
+}
