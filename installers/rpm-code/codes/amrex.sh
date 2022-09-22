ca837d67cfff4429512fb5c89228777a362d6f0c
Garret Sugarbaker <55895241+garsuga@users.noreply.github.com>
2022-09-22 15:55:37 -0600

    fix #334: add amrex installer (#338)

--- installers/rpm-code/codes/amrex.sh
+++ installers/rpm-code/codes/amrex.sh
@@ -5,5 +5,6 @@ amrex_main() {
     codes_download https://github.com/AMReX-Codes/amrex/releases/download/22.09/amrex-22.09.tar.gz amrex
     ./configure \
         --prefix="${codes_dir[prefix]}"
+    codes_make
     codes_make_install
 }
