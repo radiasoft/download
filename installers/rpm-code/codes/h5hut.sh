198e39a693ad3e41be95f4762775f6643d29cbbe
git-user <git-email>
2022-09-28 22:38:19 +0000

    fix #251: hdf5 interface has changed but h5hut has no updated so specify the old interfaces

--- installers/rpm-code/codes/h5hut.sh
+++ installers/rpm-code/codes/h5hut.sh
@@ -3,6 +3,14 @@
 h5hut_main() {
     codes_dependencies common
     codes_download https://gitlab.psi.ch/H5hut/src/-/archive/H5hut-2.0.0rc6/src-H5hut-2.0.0rc6.tar.bz2
+    # We are using an updated version of hdf5 which changed the interfaces of these functions.
+    # But, h5hut has not updated. So use the old functions (add suffix of 1)
+    # https://docs.hdfgroup.org/archive/support/HDF5/doc/RM/APICompatMacros.html
+    for f in  $(find . -name '*.c' -o -name '*.h'); do
+        for n in 'H5Oget_info' 'H5Oget_info_by_name'; do
+            perl -pi -e "s/${n}\b/${n}1/" "$f"
+        done
+    done
     perl -pi -e 's{\`which}{\`type -p}' autogen.sh
     ./autogen.sh
     perl -pi -e 's{\`which}{\`type -p}' configure
