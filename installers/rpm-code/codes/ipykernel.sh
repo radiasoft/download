5af9506467c5453824304d756a146ab7edbbf64b
Evan Carlin <evan@carlin.com>
2022-10-11 19:28:54 +0000

    new variables for python versions

--- installers/rpm-code/codes/ipykernel.sh
+++ installers/rpm-code/codes/ipykernel.sh
@@ -8,14 +8,11 @@ ipykernel_main() {
 ipykernel_python_install() {
     # http://ipython.readthedocs.io/en/stable/install/kernel_install.html
     # http://www.alfredo.motta.name/create-isolated-jupyter-ipython-kernels-with-pyenv-and-virtualenv/
-    # TODO(e-carlin):  discuss w/ rjn $1 and $2 were removed in 341c73c3667396d2b18fdf2ac3f0d0a49ce1e7c2
-    set -- '3' 'py3'
-
     local -a l=$(python -m ipykernel install \
-        --display-name "Python $1" \
-        --name "$2" \
+        --display-name "Python ${install_version_python%%.*}" \
+        --name "$install_version_python_venv" \
         --user \
-        --env PYENV_VERSION "$2")
+        --env PYENV_VERSION "$install_version_python_venv")
     l=${l[-1]}
     if [[ ! $l =~ ${codes_dir[share]} ]]; then
         install_err "expecting $l to be a subdirectory of ${codes_dir[share]}"
