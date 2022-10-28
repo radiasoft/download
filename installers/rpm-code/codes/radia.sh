009db873c2761ca99a1fd6c4e2938636e3da028a
Evan Carlin <evan@carlin.com>
2022-10-28 09:03:01 -0600

    fix #358: install trimesh (#359)

--- installers/rpm-code/codes/radia.sh
+++ installers/rpm-code/codes/radia.sh
@@ -25,6 +25,7 @@ EOF
 }
 
 radia_python_install() {
+    install_pip_install trimesh
     cd Radia/cpp/py
     MODE=mpi python setup.py build_ext
     codes_python_lib_copy "$(find . -name radia*.so)"
