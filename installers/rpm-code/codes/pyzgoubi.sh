06c2f12d82dc18340baf78d8201cd2cb6af57572
git-user <git-email>
2022-11-01 17:54:54 +0000

    Fix #364: sys no longer has attribute setcheckinterval
    In py3.2 and beyond this function didn't have any effect. In py3.9 it
    was removed. So I think it is safe to remove it here.

--- installers/rpm-code/codes/pyzgoubi.sh
+++ installers/rpm-code/codes/pyzgoubi.sh
@@ -9,4 +9,18 @@ pyzgoubi_python_install() {
 pyzgoubi_main() {
     codes_dependencies common
     codes_download https://github.com/PyZgoubi/PyZgoubi.git
+    pyzgoubi_patch
+}
+
+pyzgoubi_patch() {
+    patch zgoubi/core.py <<'EOF'
+@@ -67,7 +67,6 @@
+
+ zlog.setLevel(zgoubi_settings['log_level'])
+
+-sys.setcheckinterval(10000)
+
+ zgoubi_module_path = os.path.dirname(os.path.realpath(__file__))
+ # something like
+EOF
 }
