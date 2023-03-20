0ae5e2d354f2b393cb5004aa615ae7cd0f3d3120
e-carlin <evan@carlin.com>
2023-03-20 20:15:19 +0000

    Fix #428: Add epics-pcas module

--- installers/rpm-code/codes/epics.sh
+++ installers/rpm-code/codes/epics.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 
-epics_main() {
-    codes_dependencies common
+epics_install() {
+    declare b="${codes_dir[prefix]}"/epics
     codes_download https://epics.anl.gov/download/base/base-7.0.2.tar.gz
     cd ..
-    mv base-7.0.2 "$HOME"/.local/epics
-    cd "$HOME"/.local/epics
-    local arch=linux-x86_64
+    mv base-7.0.2 "$b"
+    cd "$b"
+    epics_pcas
+    declare a=linux-x86_64
     codes_make \
-         EPICS_HOST_ARCH="$arch" \
+         EPICS_HOST_ARCH="$a" \
          LINKER_USE_RPATH=YES \
          SHARED_LIBRARIES=YES
+    # Needs to happen here to get access to files in $b
+    EPICS_BASE="$b" EPICS_HOST_ARCH="$a" install_pip_install  pcaspy
     # leave bin because there are other files (.pl & .pm) that may be referenced
     # Some of these are large, e.g. modules
     rm -rf modules documentation html src test
     find lib -name '*.a' | xargs -n 100 rm -f
-    cd bin/"$arch"
+    cd bin/"$a"
     ls | egrep -v '^S99|\.p[lm]$' | xargs -I % install -m 555 % "${codes_dir[bin]}"
+}
+
+epics_main() {
+    codes_yum_dependencies swig
+    codes_dependencies common
+    epics_install
+}
+
+epics_pcas() {
+    declare p="$PWD"
+    mkdir modules/pcas
+    cd $_
+    codes_curl https://github.com/epics-modules/pcas/archive/refs/tags/v4.13.3.tar.gz \
+        | tar xzf - --strip-components=1
+    cd ..
+    cat > Makefile.local <<'EOF'
+SUBMODULES += pcas
+pcas_DEPEND_DIRS = libcom
+EOF
+    cd "$p"
 }
