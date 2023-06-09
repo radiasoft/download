15649455effedf7c302a6a130433042e9063b505
e-carlin <evan@carlin.com>
2023-06-09 15:58:14 +0000

    pr comments

--- installers/rpm-code/codes/geant4.sh
+++ installers/rpm-code/codes/geant4.sh
@@ -20,37 +20,43 @@ EOF
 geant4_install_data_download_script() {
     install -m 555 /dev/stdin "${codes_dir[bin]}"/rs_geant4_download_data <<'EOF'
 #!/bin/bash
+#
+# Install data sets for Geant4 from https://cern.ch/geant4-data/datasets
+#
 set -eou pipefail
+_USAGE="usage: $0 dataset-install-directory
+You must supply an existing and writable directory where you want the datasets to be installed"
 
 install_d=${1:-}
-if [[ ! $install_d ]]; then
-    echo "usage: $0 dataset-install-directory
-You must supply a directory where you want the datasets to be installed" 1>&2
+if [[ ! $install_d || ! -d $install_d || ! -w $install_d ]]; then
+    echo "$_USAGE" 1>&2
     exit 1
 fi
 
 declare datasets=(
-    'G4EMLOW.8.2'
-    'G4PhotonEvaporation.5.7'
-    'G4RadioactiveDecay.5.6'
-    'G4PARTICLEXS.4.0'
-    'G4PII.1.3'
-    'G4RealSurface.2.2'
-    'G4SAIDDATA.2.0'
-    'G4ABLA.3.1'
-    'G4INCL.1.0'
-    'G4ENSDFSTATE.2.3'
-    'G4TENDL.1.4'
+    G4EMLOW.8.2
+    G4PhotonEvaporation.5.7
+    G4RadioactiveDecay.5.6
+    G4PARTICLEXS.4.0
+    G4PII.1.3
+    G4RealSurface.2.2
+    G4SAIDDATA.2.0
+    G4ABLA.3.1
+    G4INCL.1.0
+    G4ENSDFSTATE.2.3
+    G4TENDL.1.4
 )
 
-for d in ${datasets[@]}; do
+for d in "${datasets[@]}"; do
     echo "Installing: $d"
     curl -s -S -L https://cern.ch/geant4-data/datasets/"$d".tar.gz | tar xz --directory="$install_d"
 done
 
-echo "
+cat <<EOF2
 You must set 'GEANT4_DATA_DIR=$install_d' when running geant4 for it to find the datsets
-For example: GEANT4_DATA_DIR=$install_d ./exampleB1"
+For example:
+GEANT4_DATA_DIR=$install_d ./exampleB1"
+EOF2
 EOF
 }
 
