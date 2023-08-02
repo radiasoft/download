994d56ec4eb7008c310ccbcf15d10cae03f86f38
rorour <ravenmarieorourke@gmail.com>
2023-08-02 13:23:44 -0600

    comments

--- installers/rpm-code/codes/srw.sh
+++ installers/rpm-code/codes/srw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 : to_install_at_nersc <<'EOF'
-# Hinsts for installing SRW at NERSC
+# Hints for installing SRW at NERSC
 
 ## Install python
 curl radia.run | bash -s nersc-pyenv
@@ -22,19 +22,31 @@ EOF
 srw_main() {
     codes_yum_dependencies fftw2-devel
     codes_dependencies bnlcrl ml
-    codes_download ochubar/SRW
-    # committed *.so files are not so good.
-    find . -name \*.so -o -name \*.a -o -name \*.pyd -exec rm {} \;
-    perl -pi -e "s/'fftw'/'sfftw'/" cpp/py/setup.py
-    perl -pi -e 's/-lfftw/-lsfftw/; s/\bcc\b/gcc/; s/\bc\+\+/g++/' cpp/gcc/Makefile
-    cd cpp/gcc
-    codes_make lib
 }
 
 srw_python_install() {
     install_pip_install primme
     install_pip_install srwpy==4.0.0.b1
+    srw_python_patch_srwlib
     srw_srwpy_backwards_compatible
+}
+
+srw_python_patch_srwlib() {
+    patch --quiet $(python -c 'import srwpy.srwlib; print(srwpy.srwlib.__file__)') <<'EOF'
+diff --git a/env/work/srw_python/srwlib.py b/env/work/srw_python/srwlib.py
+index 9e052df..0ab17fb 100644
+--- a/env/work/srw_python/srwlib.py
++++ b/env/work/srw_python/srwlib.py
+@@ -10267,6 +10267,8 @@ def srwl_wfr_emit_prop_multi_e(_e_beam, _mag, _mesh, _sr_meth, _sr_rel_prec, _n_
+                         #srwl_uti_save_intens_ascii(resStokes2.to_deg_coh(), meshRes2, fpdc2, _n_stokes = 1, _arLabels = resLabelsToSaveDC, _arUnits = resUnitsToSaveDC, _mutual = doMutual, _cmplx = 0) #OC12072019 # Deg. of Coh. Cut vs Y
+                         #srwl_uti_save_intens_ascii(resStokes2.to_deg_coh(), meshRes2, fpdc2, _n_stokes = 1, _arLabels = resLabelsToSaveDC, _arUnits = resUnitsToSaveDC, _mutual = 2, _cmplx = 0) #OC16072019 # Deg. of Coh. Cut vs Y
+                         srwl_uti_save_intens(resStokes2.to_deg_coh(), meshRes2, fpdc2, _n_stokes = 1, _arLabels = resLabelsToSaveDC, _arUnits = resUnitsToSaveDC, _mutual = 2, _cmplx = 0, _form = _file_form) #OC17072021 # Deg. of Coh. Cut vs Y
++                elif _char == 61 and i + 1 < nRecv:
++                    pass
+                 else:
+                     #srwl_uti_save_intens_ascii(resStokes.arS, meshRes, file_path1, numComp, _arLabels = resLabelsToSave, _arUnits = resUnitsToSave, _mutual = doMutual, _cmplx = (1 if doMutual else 0)) #OC30052017
+                     #srwl_uti_save_intens_ascii(resStokes.arS, meshRes, fp1, numComp, _arLabels = resLabelsToSave, _arUnits = resUnitsToSave, _mutual = doMutual, _cmplx = (1 if doMutual else 0)) #OC14082018
+EOF
 }
 
 srw_srwpy_backwards_compatible() {
@@ -61,23 +73,23 @@ srw_srwpy_backwards_compatible() {
         uti_plot_matplotlib
     )
 
-    cd /home/vagrant/.pyenv/versions/3.9.15/envs/py3/lib/python3.9/site-packages/
-        cat > srwpy_import_warning.py <<EOF
+    declare d=$(codes_python_lib_dir)
+    install -m 444 /dev/stdin $d/srwpy_import_warning.py <<EOF
 displayed = False
 
 def check(calling_module):
     global displayed
     if not displayed:
         displayed = True
-        raise DeprecationWarning(f"This method of calling {calling_module} is deprecated. Please change to import from srwpy (ex: import {calling_module} -> from srwpy import {calling_module})")
+        print(f"This method of importing {calling_module} is deprecated. Please change to import from srwpy (ex: import {calling_module} -> from srwpy import {calling_module})")
 EOF
-    for i in "${old_modules[@]}"
-    do
-        cat > $i.py <<EOF
-from srwpy.$i import *
+    declare m
+    for m in "${old_modules[@]}"; do
+        install -m 444 /dev/stdin $d/$m.py <<EOF
+from srwpy.$m import *
 import srwpy_import_warning
 
-srwpy_import_warning.check("$i")
+srwpy_import_warning.check("$m")
 EOF
     done
 }
