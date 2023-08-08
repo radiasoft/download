96f20af1949286da6d98b7eb4cae0acd2f8c0213
Raven <ravenmarieorourke@gmail.com>
2023-08-08 09:45:35 -0600

    fix #487: srwpy now uses main SRW repo (#512)

--- installers/rpm-code/codes/srw.sh
+++ installers/rpm-code/codes/srw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 : to_install_at_nersc <<'EOF'
-# Hinsts for installing SRW at NERSC
+# Hints for installing SRW at NERSC
 
 ## Install python
 curl radia.run | bash -s nersc-pyenv
@@ -22,28 +22,17 @@ EOF
 srw_main() {
     codes_yum_dependencies fftw2-devel
     codes_dependencies bnlcrl ml
-    codes_download ochubar/SRW
-    # committed *.so files are not so good.
-    find . -name \*.so -o -name \*.a -o -name \*.pyd -exec rm {} \;
-    perl -pi -e "s/'fftw'/'sfftw'/" cpp/py/setup.py
-    perl -pi -e 's/-lfftw/-lsfftw/; s/\bcc\b/gcc/; s/\bc\+\+/g++/' cpp/gcc/Makefile
-    cd cpp/gcc
-    codes_make lib
 }
 
 srw_python_install() {
     install_pip_install primme
-    cd SRW/cpp/py
-    make python
-    cd ../..
+    install_pip_install srwpy==4.0.0.b1
     srw_python_patch_srwlib
-    srw_python_patch_uti_math_eigen
-    codes_python_lib_copy env/work/srw_python/{{srwl,uti}*.py,srwlpy*.so}
-    find . -name srwlpy\*.so -exec rm {} \;
+    srw_srwpy_backwards_compatible
 }
 
 srw_python_patch_srwlib() {
-    patch --quiet env/work/srw_python/srwlib.py <<'EOF'
+    patch --quiet $(python -c 'import srwpy.srwlib; print(srwpy.srwlib.__file__)') <<'EOF'
 diff --git a/env/work/srw_python/srwlib.py b/env/work/srw_python/srwlib.py
 index 9e052df..0ab17fb 100644
 --- a/env/work/srw_python/srwlib.py
@@ -60,16 +49,49 @@ index 9e052df..0ab17fb 100644
 EOF
 }
 
-srw_python_patch_uti_math_eigen() {
-    patch --quiet env/work/srw_python/uti_math_eigen.py <<'EOF'
-@@ -29,7 +29,7 @@
- #from sklearn.decomposition import PCA as PCA #OC04082021 (moved to try-except)
- try:
-     from scipy.linalg import eigh as largest_eigh
--    from scipy.sparse.linalg.eigen.arpack import eigsh as largest_eigsh
-+    from scipy.sparse.linalg import eigsh as largest_eigsh
- except:
-     print("UtiMathEigen WARNING: SciPy unavailable; to install: pip install scipy")
- try:
+srw_srwpy_backwards_compatible() {
+    declare -a old_modules=(
+        srwl_bl
+        srwl_uti_brightness
+        srwl_uti_cryst
+        srwl_uti_mag
+        srwl_uti_smp
+        srwl_uti_smp_rnd_obj2d
+        srwl_uti_smp_rnd_obj3d
+        srwl_uti_src
+        srwl_uti_und
+        srwlib
+        srwlpy
+        uti_io
+        uti_io_genesis
+        uti_mag
+        uti_math
+        uti_math_eigen
+        uti_parse
+        uti_plot
+        uti_plot_com
+        uti_plot_matplotlib
+    )
+
+    declare d=$(codes_python_lib_dir)
+    install -m 444 /dev/stdin $d/srwpy_import_warning.py <<EOF
+import sys
+
+displayed = False
+
+def check(calling_module):
+    global displayed
+    if not displayed:
+        displayed = True
+        print(f"This method of importing {calling_module} is deprecated. Please change to import from srwpy (ex: import {calling_module} -> from srwpy import {calling_module})", file=sys.stderr)
 EOF
+    declare m
+    for m in "${old_modules[@]}"; do
+        install -m 444 /dev/stdin $d/$m.py <<EOF
+from srwpy.$m import *
+import srwpy_import_warning
+
+srwpy_import_warning.check("$m")
+EOF
+    done
 }
