2ac09889366cc6318bf0cf92cd8746abe5b97fd8
robnagler <github@q33.us>
2023-09-14 23:04:01 +0000

    review comments

--- installers/rpm-code/dnf-download.sh
+++ installers/rpm-code/dnf-download.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
+#
+# Download rscode-*.rpm from system dnf repo
+#
+set -e
+declare code=$1
 set +euo pipefail
 source ~/.bashrc
 set -euo pipefail
 source ./dev-env.sh
-declare f
-sudo dnf makecache
-declare d=dnf-download-tmp
-rm -rf "$d"
-mkdir "$d"
-declare common=0
-for f in "$@"; do
-    dnf download --downloaddir "$d" "rscode-$f"
-    if [[ $1 == common ]]; then
-        common=1
-        cd ~/src/radiasoft/container-rpm-code
-        if [[ ! $(docker images | grep radiasoft/fedora) ]]; then
-            cd container-fedora
-            # in case set by dev-env.sh, because server isn't running yet
-            radia_run container-build
-        fi
-        radia_run container-build
-    fi
-done
-mv "$d"/*.rpm "$rpm_code_install_dir"
+dnf download --downloaddir "$rpm_code_install_dir" "rscode-$code" || true
 createrepo -q --update "$rpm_code_install_dir"
-if (( common )); then
+if [[ $code == common ]]; then
     cd ~/src/radiasoft/container-rpm-code
     if [[ ! $(docker images | grep radiasoft/fedora) ]]; then
-        cd container-fedora
-        # in case set by dev-env.sh, because server isn't running yet
-        radia_run container-build
+        docker pull radiasoft/fedora
     fi
-    radia_run container-build
+    install_server= radia_run container-build
 fi
