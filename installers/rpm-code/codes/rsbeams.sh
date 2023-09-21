d8788c7d81d05b05319bf35a1533d8b903826fb4
Rob Nagler <github@q33.us>
2023-09-21 15:12:26 -0400

    Fix #543 nlopt conflict; cadopenmc depends on openmc (#544)

--- installers/rpm-code/codes/rsbeams.sh
+++ installers/rpm-code/codes/rsbeams.sh
@@ -26,7 +26,7 @@ rsbeams_init_vars() {
 }
 
 rsbeams_python_install() {
-    install_pip_install nlopt DFO-LS Libensemble yt
+    install_pip_install DFO-LS Libensemble yt
     declare r
     for r in "${_rsbeam_codes[@]}"; do
         cd "$r"
