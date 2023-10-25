96208dd91fe98902427aca1b6c892e428cff8bda
Raven <ravenmarieorourke@gmail.com>
2023-10-25 15:34:23 -0600

    fix #562: add --no-cache-dir to pip install to free space during ci run (#561)
    * add --no-cache-dir to pip install to free space during ci run
    
    * clarifying comment
    
    * issue number

--- installers/python-ci/radiasoft-download.sh
+++ installers/python-ci/radiasoft-download.sh
@@ -50,7 +50,9 @@ python_ci_main() {
                 pip install git+https://'${GITHUB_TOKEN:+$GITHUB_TOKEN@}'github.com/radiasoft/\$x.git
             done
             pip uninstall -y '$r' >& /dev/null || true
-            pip install -e .
+            # GitHub CI runners are limited on disk space so use --no-cache-dir to limit disk usage as much as
+            # possible. See git.radiasoft.org/downloads/issues/562
+            pip install --no-cache-dir -e .
             export PYKERN_PKCLI_TEST_MAX_FAILURES=1 PYKERN_PKCLI_TEST_RESTARTABLE=1
             if [[ -f test.sh ]]; then
                 bash test.sh
