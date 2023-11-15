e46a6eaeb9b2f6efd08e702457ddea44ece43a82
Evan Carlin <evan@carlin.com>
2023-11-15 11:41:47 -0700

    Fix #572: update flash to 4.7.1 (#573)

--- installers/flash-tarball/radiasoft-download.sh
+++ installers/flash-tarball/radiasoft-download.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -euo pipefail
 
-_flash_tarball_version=4.6.2
+_flash_tarball_version=4.7.1
 
 flash_tarball_main() {
-    local d=$PWD/proprietary
-    local t=$d/FLASH$_flash_tarball_version.tar.gz
+    declare d=$PWD/proprietary
+    declare t=$d/FLASH$_flash_tarball_version.tar.gz
     if [[ ! -f $t ]]; then
             install_err "$t must exist"
     fi
     install_tmp_dir
-    local p=$PWD
-    local r=()
+    declare p=$PWD
+    declare r=()
     install_url radiasoft/download installers
     install_script_eval rpm-code/codes.sh
     codes_download flashcap
     git fetch --unshallow
-    local x
+    declare x
     for x in \
         'CapLaser3D 47a641aa467ff48c1337a69e6c3a6778e5b854ae' \
         'CapLaserBELLA master' \
@@ -39,12 +39,12 @@ flash_tarball_main() {
 }
 
 flash_tarball_patch_and_update_tgz() {
-    local src_tgz=$1
-    local b=FLASH$_flash_tarball_version
+    declare src_tgz=$1
+    declare b=FLASH$_flash_tarball_version
     tar xzf "$src_tgz"
     cd "$b"
     # POSIT: Fedora using mpich
-    local d=/usr/lib64/mpich
+    declare d=/usr/lib64/mpich
     patch --quiet sites/Prototypes/Linux/Makefile.h <<EOF
 6c6
 < MPI_PATH   = /usr/local/mpich2/
@@ -62,36 +62,21 @@ flash_tarball_patch_and_update_tgz() {
 > F90FLAGS = -fallow-argument-mismatch
 EOF
 
-    # The following patches both contain trailing whitespace because
-    # the original source file contains it. Keeping the trailing
-    # whitespace makes the patch smaller which makes the important
-    # change in the patch easier to see.
-    patch --quiet bin/setup.py <<'EOF'
-@@ -13,7 +13,7 @@
- 
- ########################### START SCRIPT ################################
- def main():
--    sys.setcheckinterval(10000) # this is not threaded application
-+    sys.setswitchinterval(10000) # this is not threaded application
- 
-     parseCmd.init()
-     # setup.py is FLASH_HOME/bin/setup.py
-EOF
     patch --quiet tools/python/flmake/setup.py <<'EOF'
 @@ -32,7 +32,7 @@
      global RUNTIME_FILES
- 
+
      # this is not a threaded application
 -    sys.setcheckinterval(10000)
 +    sys.setswitchinterval(10000)
      cwdir = os.getcwd()
- 
+
      # setup.py is FLASH_HOME/bin/setup.py
 EOF
     cd ..
     mv "$b" "flash"
     # POSIT: Matches sirepo.sim_data.flash._flash_src_tarball_basename
-    local r=flash.tar.gz
+    declare r=flash.tar.gz
     tar czf "$r" flash
     echo "$r"
 }
