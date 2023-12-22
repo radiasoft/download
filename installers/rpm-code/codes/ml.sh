c89c4090900f4211f269144d1d84bda941d9ac56
Evan Carlin <evan@carlin.com>
2023-12-22 11:22:58 -0700

    Fix #590: update tensorflow to 2.15.0 (#591)
    - update tensorflow to 2.15.0
    - xraylib from rscode-xraylib conflicted with rscode-bluesky so it was moved into common.
    After this move there was nothing left in xraylib.sh so it was removed
    - Removed some feature tests for f36 vs f32. There are tests in other places but I removed
    ones directly related to my work. We are going to be fully on f36 so all tests can be removed.
    - update h5py to 3.10.0. This wasn't strictly necessary. Tensorflow can use and older h5py
    but since I was in here updating tensorflow I figured it was good to update related packages.

--- installers/rpm-code/codes/ml.sh
+++ installers/rpm-code/codes/ml.sh
@@ -7,74 +7,36 @@ ml_main() {
 
 ml_python_install() {
     install_pip_install h5ImageGenerator
-    if install_version_fedora_lt_36; then
-        ml_python_install_f32
-        return
-    fi
-    ml_python_install_f36
-}
-
-ml_python_install_f32() {
-    declare x=(
-        'absl-py>=0.7.0'
-        'gast==0.3.3'
-        'google-pasta>=0.1.8'
-        'grpcio>=1.8.6'
-        'keras-preprocessing<1.2,>=1.1.1'
-        'opt-einsum>=2.3.2'
-        'protobuf>=3.9.2'
-        'tensorboard<3,>=2.3.0'
-        'google-auth<2,>=1.6.3'
-        'cachetools<5.0,>=2.0.0'
-        'google-auth-oauthlib<0.5,>=0.4.1'
-        'markdown>=2.6.8'
-        'pyasn1-modules>=0.2.1'
-        'pyasn1<0.5.0,>=0.4.6'
-        'requests-oauthlib>=0.7.0'
-        'oauthlib>=3.0.0'
-        'rsa<5,>=3.1.4'
-        'tensorboard-plugin-wit>=1.6.0'
-        'tensorflow-estimator<2.4.0,>=2.3.0'
-        'termcolor>=1.1.0'
-        'werkzeug>=0.11.15'
-    )
-    install_pip_install "${x[@]}"
-    install_pip_install --no-deps tensorflow==2.3.1
-    install_pip_install keras==2.4.3 scikit-learn
-
-}
-
-
-ml_python_install_f36() {
-    # sympy is needed for webcon and rsbeams
-    # scikit-image is need for srw
-
+    # The tensorflow version we install must be supported by the CUDA version we have installed
+    # https://www.tensorflow.org/install/source#gpu
     # deps copied from tensorflow/tools/pip_package/setup.py
     local x=(
-        'absl-py>=1.0.0'
-        'astunparse>=1.6.0'
-        'flatbuffers>=2.0'
-        'gast>=0.2.1,<=0.4.0'
-        'google_pasta>=0.1.1'
-        'grpcio>=1.24.3,<2.0' # sys.byteorder == 'little' on our systems
-        'h5py>=2.9.0'
-        'keras>=2.10.0,<2.11'
-        'keras_preprocessing>=1.1.1'
-        'libclang>=13.0.0'
-        'numpy>=1.20'
-        'opt_einsum>=2.3.2'
-        'packaging'
-        'protobuf>=3.9.2,<3.20'
-        'setuptools'
-        'six>=1.12.0'
-        'tensorboard>=2.10,<2.11'
-        'tensorflow-io-gcs-filesystem>=0.23.1'
-        'tensorflow_estimator>=2.10.0,<2.11'
-        'termcolor>=1.1.0'
-        'typing_extensions>=3.6.6'
+    'absl-py>=1.0.0'
+    'astunparse>=1.6.0'
+    'flatbuffers>=23.5.26'
+    'gast>=0.2.1,!=0.5.0,!=0.5.1,!=0.5.2'
+    'google_pasta>=0.1.1'
+    'h5py>=2.9.0'
+    'libclang>=13.0.0'
+    'ml_dtypes~=0.2.0'
+    'numpy>=1.23.5,<2.0.0'
+    'opt_einsum>=2.3.2'
+    'packaging'
+    'protobuf>=3.20.3,<5.0.0dev,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5'
+    'setuptools'
+    'six>=1.12.0'
+    'termcolor>=1.1.0'
+    'typing_extensions>=3.6.6'
+    'wrapt>=1.11.0,<1.15'
+    'tensorflow-io-gcs-filesystem>=0.23.1'
+    'grpcio>=1.24.3,<2.0' # sys.byteorder == 'little' on our systems
+    'tensorboard>=2.15,<2.16'
+    'tensorflow_estimator>=2.15.0,<2.16'
+    'keras>=2.15.0,<2.16'
     )
     install_pip_install "${x[@]}"
-    install_pip_install --no-deps tensorflow==2.10.0
-    install_pip_install scikit-learn
-
+    install_pip_install --no-deps tensorflow==2.15.0
+    # scikit is need for srw
+    # sympy is needed for webcon and rsbeams
+    install_pip_install scikit-learn sympy
 }
