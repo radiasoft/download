c580bb94bd9968bc302a8c5840976065ea8c664e
Evan Carlin <evan@carlin.com>
2024-01-18 13:10:16 -0700

    Fix #601: create rscode-julia (#602)

---
+++ installers/rpm-code/codes/julia.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+julia_main() {
+    codes_dependencies common
+    declare v=julia-1.9.3
+    declare d=${codes_dir[prefix]}/$v
+    install -d -m 755 "$d"
+    codes_curl https://julialang-s3.julialang.org/bin/linux/x64/1.9/"$v"-linux-x86_64.tar.gz \
+        | tar xz --strip-components=1 -C "$d"
+    chmod -R a+rX "$d"
+    ln -s "$d"/bin/julia "${codes_dir[bin]}"/julia
+}
