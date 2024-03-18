dbdf629ddd884fe8293e7d5ee7edc25e03380521
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2024-03-18 17:05:36 -0600

    Fix #618 rename to chronver (#619)

--- installers/python-ci/radiasoft-download.sh
+++ installers/python-ci/radiasoft-download.sh
@@ -13,7 +13,7 @@ python_ci_main() {
         pykern)
             i=radiasoft/python3
             ;;
-        rsaccounting|rsconf|rschronver|rslearn)
+        rsaccounting|rsconf|chronver|rslearn)
             i=radiasoft/python3
             p+=( pykern )
             ;;
