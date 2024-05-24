a4f5334e3569f2ccc51d0d0abc3141358c230d1b
Raven <ravenmarieorourke@gmail.com>
2024-05-24 09:12:32 -0600

    fix #555: check for necessary repos in dev-build.sh (#681)

--- installers/rpm-code/dev-build.sh
+++ installers/rpm-code/dev-build.sh
@@ -3,6 +3,14 @@ set +euo pipefail
 source ~/.bashrc
 set -euo pipefail
 source ./dev-env.sh
+
+for r in container-rpm-code container-fedora; do
+    if [[ ! -d ~/src/radiasoft/$r ]]; then
+        echo "~/src/radiasoft/$r not found. You may need to run $PWD/dev-server.sh."
+        exit 1
+    fi
+done
+
 if ! radia_run rpm-code "$@"; then
     # this is a good guess, but there are other problems
     echo "you may need to start your server:
