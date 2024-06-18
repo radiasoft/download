44ddaeef75fa1152b4fe1ba9111656f1f4b2880f
Evan Carlin <evan@carlin.com>
2024-06-18 15:07:31 -0600

    Fix #691: add lume-impact (#692)

--- installers/rpm-code/codes/impactt.sh
+++ installers/rpm-code/codes/impactt.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 impactt_main() {
-    codes_dependencies common
+    # openpmdapi provides openpmd-beamphysics which is required by lume-base
+    # pydicom is required by one of lume-base deps
+    codes_dependencies common openpmdapi pydicom
     codes_download https://github.com/impact-lbl/impact-t.git
     cd src
     codes_cmake2 -DCMAKE_INSTALL_PREFIX="${codes_dir[prefix]}"
@@ -10,4 +12,14 @@ impactt_main() {
     codes_cmake2 -DCMAKE_INSTALL_PREFIX="${codes_dir[prefix]}" -DUSE_MPI=ON
     codes_cmake_build install
     codes_cmake_clean
+}
+
+impactt_python_install() {
+    # Requirements for lume-impact aren't installed in a pip install.
+    # https://github.com/ChristopherMayes/lume-impact/blob/5593a87d722a91d3d66df2804731a281fddd3aa1/pyproject.toml#L20
+    codes_download https://github.com/ColwynGulliford/distgen.git v2.0.2
+    codes_python_install
+    install_pip_install lume-base==0.3.2
+    codes_download https://github.com/ChristopherMayes/lume-impact.git 5593a87d722a91d3d66df2804731a281fddd3aa1
+    codes_python_install
 }
