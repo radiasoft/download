85490d1ed66d048c7758eaa9d373a4916fe48c28
Evan Carlin <evan@carlin.com>
2024-11-18 17:56:54 +0000

    split out installers so rsconf can call just redhat-docker-install

--- installers/redhat-docker/radiasoft-download.sh
+++ installers/redhat-docker/radiasoft-download.sh
@@ -32,27 +32,7 @@ Then re-run this command. This should only happen in development environments.
         perl -pi -e 's{(?<=^SELINUX=).*}{disabled}' /etc/selinux/config
         install_err 'Disabled selinux. You need to "vagrant reload", then re-run this installer'
     fi
-    if install_os_is_fedora; then
-        install_yum_install dnf-plugins-core
-        dnf -q config-manager \
-            --add-repo \
-            https://download.docker.com/linux/fedora/docker-ce.repo
-    elif install_os_is_centos_7; then
-        yum-config-manager \
-            --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-        yum makecache fast
-        install_yum_install yum-plugin-ovl
-    elif install_os_is_rhel_compatible; then
-        dnf -q config-manager \
-            --add-repo \
-            https://download.docker.com/linux/rhel/docker-ce.repo
-    else
-        install_err "installer does not support os=$install_os_release_id"
-    fi
-    install_yum_install docker-ce
-    if [[ ${redhat_docker_no_local_setup:-} ]]; then
-        return 0
-    fi
+    install_repo_eval redhat-docker-install
     usermod -aG docker vagrant
     install -d -m 700 /etc/docker
     mkdir -p "$data"
