53aed50bcbf688eed67f224f82794292667aec2e
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-06-02 10:00:49 -0600

    Fix #760 support dnf5 and support fedora 42 in general (#763)
    - Fix #761 added /etc/profile.d/rs-redhat-base.sh which defines RADIA_RUN_SERVER
    - Fix #758 do not check box updates in vagrant-dev
    - Improved run-time error handling (prints stack)
    - Fixed errexit suppression issues
    - e/fgrep references removed
    - Fix #764 vagrant-dev support libvirt forwarding (`:libvirt__forward_mode: "open"`)

--- bin/index.sh
+++ bin/index.sh
@@ -2,16 +2,14 @@
 #
 # See https://github.com/radiasoft/download
 #
-set -e -o pipefail
+set -euo pipefail
+shopt -s nullglob
 
 index_main() {
     declare -a a=()
-    if [[ ${install_server:-} && $install_server != github ]]; then
-        a=(
-            --location
-            "$install_server/radiasoft/download/bin/install.sh"
-        )
-    else
+    # POSIT: install.sh defaults to radia.run
+    declare u=${install_server:-https://radia.run}
+    if [[ $u == github ]]; then
         if [[ ${GITHUB_TOKEN:-} ]]; then
             a+=( --header "Authorization: Bearer $GITHUB_TOKEN" )
         fi
@@ -19,8 +17,10 @@ index_main() {
             --header 'Accept: application/vnd.github.raw'
             https://api.github.com/repos/radiasoft/download/contents/bin/install.sh
         )
+    else
+        a+=( "$u/radiasoft/download/bin/install.sh" )
     fi
-    curl --fail --silent --show-error "${a[@]}" | bash ${install_debug:+-x} -s "$@"
+    curl --fail --location --show-error --silent "${a[@]}" | install_server="$u" bash "${install_debug:+-x}" -s "$@"
 }
 
 index_main "$@"
--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #
-# Usage: curl https://depot.radiasoft.org | bash -s [repo args...]
+# Usage: curl https://radia.run | bash -s repo [args...]
 #
 # Find repos in radiasoft/download/installers or radiasoft/*/radiasoft-download.sh
 # or any repo with a radiasoft-download.sh in its root.
 #
 set -euo pipefail
+
+# Define all variables in functions. All functions must be prefixed with install_
 
 install_args() {
     while [[ ${1:-} ]]; do
@@ -41,7 +43,8 @@ install_args() {
         set -x
     fi
     if [[ ! $install_repo ]]; then
-        install_repo=$install_default_repo
+        install_err "Usage: curl $install_server | bash -s repo [args...]
+Must supply repo argument"
     fi
 }
 
@@ -89,6 +92,34 @@ install_download() {
     curl ${x[@]+"${x[@]}"} "$url"
 }
 
+install_export_this_script() {
+    # POSIT: all functions prefixed by install_
+    declare f
+    echo 'set -eou pipefail'
+    install_vars_export
+    for f in $(compgen -A function install_); do
+        declare -f "$f"
+    done
+    echo 'install_main "$@"'
+}
+
+install_file_from_stdin() {
+    # read stdin (here doc), install in tgt if it is new or content or permissions changed
+    # mode is a number (compatible with stat %a); user & group are names
+    # Implementation can be used in subshells with: $(declare -f install_file_from_stdin)
+    # so keep implementation free of any but the simplest dependencies
+    declare mode=$1
+    declare owner=$2
+    declare group=$3
+    declare tgt=$4
+    # bash read -r -d '' adds an extra newline, because it reads until null
+    declare src=$(cat)
+    if ! cmp -s "$tgt" - <<<"$src" || [[ $(stat --format '%a %U %G' "$tgt") != "$mode $owner $group" ]]; then
+        # Can't use --compare, because stdin
+        install --mode="$mode" --owner="$owner" --group="$group" --no-target-directory /dev/stdin "$tgt" <<<"$src"
+    fi
+}
+
 install_foss_server() {
     # foss is best served from depot_sever, because the sources
     # are static and large. You can override this by setting
@@ -120,20 +151,40 @@ install_git_clone() {
 }
 
 install_err() {
+    declare -a msg=( "$@" )
     trap - EXIT
-    if [[ -n $1 ]]; then
-        install_msg "$*
+    install_err_stack "${FUNCNAME[@]:-}"
+    if (( ${#msg[@]} > 0 )); then
+        install_msg "${msg[*]}
 If you don't know what to do, please contact support@radiasoft.net."
     fi
     if [[ -z $install_verbose ]]; then
         install_clean >& /dev/null
     fi
+    # POSIT: errexit compatible with calling functions
     exit 1
+}
+
+install_err_stack() {
+    if (( $# <= 1)); then
+        return
+    fi
+    declare funcs=( "$@" )
+    if [[ "${funcs[1]}" == install_err_trap ]]; then
+        # install_err called by install_err_trap, stack already printed
+        return
+    fi
+    declare f
+    install_msg 'bash stack:'
+    for f in "${funcs[@]:1}"; do
+        install_msg "  $f"
+    done
 }
 
 install_err_trap() {
     set +e
     trap - EXIT
+    install_err_stack "${FUNCNAME[@]:-}"
     if [[ -z $install_verbose ]]; then
         tail -10 "$install_log_file"
     fi
@@ -168,48 +219,46 @@ install_log() {
 }
 
 install_init_vars() {
+    install_init_vars_servers
+    install_init_vars_basic_options
+    install_init_vars_basic
+    install_init_vars_versions
+    install_init_vars_virt
+    install_init_vars_oci
+    eval "$(install_vars_export)"
+}
+
+install_init_vars_basic() {
+    install_clean_cmds=()
+    install_curl_flags=( --fail --location --silent --show-error )
     # For error messages
     install_log_file=$PWD/radia-run-install.log
     if ! dd if=/dev/null of="$install_log_file" 2>/dev/null; then
         install_log_file=/tmp/$(date +%Y%m%d%H%M%S)-$RANDOM-$(basename "$install_log_file")
     fi
-    install_clean_cmds=()
-    install_channel_is_default=
-    if [[ ! ${install_channel-} ]]; then
-        install_channel=prod
-        install_channel_is_default=1
-    fi
-    install_init_vars_os_release
-    install_init_vars_virt
-    : ${install_debug:=}
-    : ${install_default_repo:=container-run}
-    : ${install_server:=github}
-    : ${install_tmp_dir:=/var/tmp}
-    : ${install_verbose:=}
-    : ${install_proprietary_key:=missing-proprietary-key}
-    install_curl_flags=( -L -s -S )
     install_extra_args=()
+    install_prog="curl $install_server | bash -s"
     install_repo=
     install_script_dir=
-    install_url=
     if [[ ! -w $install_tmp_dir ]]; then
         install_tmp_dir=/var/tmp
     fi
     if [[ ! -w ${TMPDIR:-/tmp} ]]; then
         unset TMPDIR
     fi
-    if [[ $install_server =~ ^file://(/.+) && ! -r ${BASH_REMATCH[1]}/radiasoft/download ]]; then
-        install_server=github
+    install_url=
+}
+
+install_init_vars_basic_options() {
+    install_channel_is_default=
+    : ${install_tmp_dir:=/var/tmp}
+    : ${install_debug:=}
+    if [[ ! ${install_channel-} ]]; then
+        install_channel=prod
+        install_channel_is_default=1
     fi
-    : ${install_depot_server:=https://depot.radiasoft.org}
-    install_prog="curl $install_depot_server | bash -s"
-    # These vars are only used in a few cases, e.g. vagrant-dev and pyenv
-    : ${install_version_fedora:=36}
-    : ${install_version_python:=3.9.15}
-    : ${install_version_python_venv:=py${install_version_python%%.*}}
-    : ${install_version_centos:=7}
-    install_init_vars_oci
-    eval "$(install_vars_export)"
+    : ${install_proprietary_key:=missing-proprietary-key}
+    : ${install_verbose:=}
 }
 
 install_init_vars_oci() {
@@ -229,9 +278,13 @@ install_init_vars_oci() {
     export RADIA_RUN_OCI_CMD
 }
 
-install_init_vars_os_release() {
+install_init_vars_versions() {
     declare x=/etc/os-release
-    # always update
+    : ${install_version_fedora:=36}
+    : ${install_version_python:=3.9.15}
+    : ${install_version_python_venv:=py${install_version_python%%.*}}
+    : ${install_version_centos:=7}
+    # always set these vars
     if [[ -r $x ]]; then
         export install_os_release_id=$(source "$x"; echo "${ID,,}")
         export install_os_release_version_id=$(source "$x"; echo "$VERSION_ID")
@@ -244,6 +297,19 @@ install_init_vars_os_release() {
     else
         # Have something legal; unlikely to get here
         export install_os_release_version_id=0
+    fi
+}
+
+install_init_vars_servers() {
+    # POSIT: index.sh defaults to radia.run
+    declare s=https://radia.run
+    : ${install_server:=$s}
+    if [[ ! ${install_depot_server:-} ]]; then
+        if [[ $install_server == github ]]; then
+            install_depot_server=$s/depot
+        else
+            install_depot_server=$install_server/depot
+        fi
     fi
 }
 
@@ -344,34 +410,18 @@ install_repo_as_root() {
 install_repo_as_user() {
     declare user=$1
     shift
-    declare sudo=
-    if [[ $(id -u -n) == $user ]]; then
-        # passing env vars on the command line is
-        # tricky so this is the easiest way
-        (
-            install_url radiasoft/download bin
-            install_download index.sh
-        ) | install_server="$install_server" \
-            install_channel="$install_channel" \
-            install_debug="$install_debug" \
-            install_depot_server="$install_depot_server" \
-            bash -l -s "$@"
+    declare s=$(install_export_this_script)
+    if [[ $(id -u "$user") == $EUID ]]; then
+        # environment is cascaded so no need for --login
+        bash -s "$@" <<<"$s"
         return
     fi
-    (
-        install_url radiasoft/download bin
-        install_download index.sh
-    ) | (
-        # Current directory might be inaccessible
-        cd /
-        install_sudo "--user=$user" \
-            install_server="$install_server" \
-            install_channel="$install_channel" \
-            install_debug="$install_debug" \
-            install_depot_server="$install_depot_server" \
-            install_version_centos="$install_version_centos" \
-            bash -l -s "$@"
-    )
+    # Current directory might be inaccessible inside sudo
+    declare prev_pwd=$PWD
+    cd /
+    # Need login environment for the user
+    install_sudo "--user=$user" bash --login -s "$@" <<<"$s"
+    cd "$prev_pwd"
 }
 
 install_repo_eval() {
@@ -379,56 +429,13 @@ install_repo_eval() {
     # don't run in a subshell so can add to environment,
     # but don't override these vars.
     install_extra_args=() \
+        install_depot_server="$install_depot_server" \
         install_repo= \
         install_script_dir="$install_script_dir" \
         install_server="$install_server" \
-        install_depot_server="$install_depot_server" \
         install_url= \
         install_repo_internal "$@"
     cd "$prev_pwd"
-}
-
-install_script_eval() {
-    declare script=$1
-    if [[ ! $install_script_dir ]]; then
-        declare pwd=$PWD
-        install_tmp_dir
-        install_script_dir=$PWD
-        cd "$pwd"
-    fi
-    declare source=$install_script_dir/$(date +%Y%m%d%H%M%S)-$(basename "$script")
-    install_download "$script" > "$source"
-    if [[ ! -s $source ]]; then
-        install_err
-    fi
-    if [[ ! $(head -1 "$source") =~ ^#! ]]; then
-        install_err "$script: no #! at start of file: $source"
-    fi
-    declare m=
-    if [[ "$script" == radiasoft-download.sh ]]; then
-        declare f
-        # POSIT: same special case in install_url()
-        if [[ $install_url =~ ^https://api.github.com/repos/[^/]+/([^/]+)/contents$ ]]; then
-            f=${BASH_REMATCH[1]}
-        else
-            f=$(basename "$install_url")
-        fi
-        # before sourcing, which can modify anything, do this calculation
-        f=${f//-/_}_main
-        # three cases: main without args or with install_extra_args
-        # Be loose in case there's a bug. Compliant scripts must
-        # not call main in any form
-        if ! egrep "^$f( *| .*@.*)$" "$source" >&/dev/null; then
-            m=$f
-            # Just in case repo was evaled already
-            unset "$m"
-        fi
-    fi
-    install_info "Source: $source"
-    source "$source"
-    if [[ $m && $(type -t "$m") == function ]]; then
-        $m ${install_extra_args[@]+"${install_extra_args[@]}"}
-    fi
 }
 
 install_repo_internal() {
@@ -464,6 +471,52 @@ install_repo_internal() {
     fi
     install_url "$first" "$rest"
     install_script_eval radiasoft-download.sh
+}
+
+install_script_eval() {
+    declare script=$1
+    if [[ ! $install_script_dir ]]; then
+        declare pwd=$PWD
+        install_tmp_dir
+        install_script_dir=$PWD
+        cd "$pwd"
+    fi
+    declare source=$install_script_dir/$(date +%Y%m%d%H%M%S)-$(basename "$script")
+    install_download "$script" > "$source"
+    if [[ ! -s $source ]]; then
+        install_err "error downloading script=$script"
+    fi
+    if [[ ! $(head -1 "$source") =~ ^#! ]]; then
+        install_err "$script: no #! at start of file: $source"
+    fi
+    # Caculate main function before sourcing
+    declare m=$(basename "$script" .sh)
+    if [[ "$script" == radiasoft-download.sh ]]; then
+        declare f
+        # POSIT: same special case in install_url()
+        if [[ $install_url =~ ^https://api.github.com/repos/[^/]+/([^/]+)/contents$ ]]; then
+            f=${BASH_REMATCH[1]}
+        else
+            f=$(basename "$install_url")
+        fi
+        m=${f//-/_}
+    fi
+    m+=_main
+    # three cases: main without args or with install_extra_args
+    # Be loose in case there's a bug. Compliant scripts must
+    # not call main in any form
+    if grep -E "^$m( *| .*@.*)$" "$source" >&/dev/null; then
+        # main is called in script so don't call again
+        m=
+    else
+        # Just in case repo was evaled already, get a fresh definition of main
+        unset "$m"
+    fi
+    install_info "Source: $source"
+    source "$source"
+    if [[ $m && $(type -t "$m") == function ]]; then
+        $m ${install_extra_args[@]+"${install_extra_args[@]}"}
+    fi
 }
 
 install_source_bashrc() {
@@ -514,44 +567,82 @@ usage: $install_prog [verbose|quiet] [<installer>|*/*] [extra args]"
 }
 
 install_vars_export() {
-    for f in install_server \
-        install_channel \
-        install_debug \
-        install_depot_server \
-        install_proprietary_key \
-        install_version_fedora \
-        install_version_python \
-        install_version_centos \
-        $(compgen -A variable RADIA_RUN_) \
+    declare f
+    declare x=(
+        install_server
+        install_channel
+        install_debug
+        install_depot_server
+        install_proprietary_key
+        install_version_fedora
+        install_version_python
+        install_version_centos
+        $(compgen -A variable RADIA_RUN_)
         $(compgen -A variable GITHUB_)
-    do
+    )
+    for f in "${x[@]}"; do
         export "$f"
         echo "$(declare -p $f);"
     done
 }
 
 install_version_fedora_lt_36() {
-    if (( $install_version_fedora < 36 )); then
-        return 0
-    fi
-    return 1
+    (( $install_version_fedora < 36 ))
 }
 
 install_yum() {
     declare args=( "$@" )
+    declare cmd=$1
     declare yum=yum
-    if [[ $(type -t dnf) ]]; then
-        yum=dnf
+    declare flags=( -y )
+    if [[ $(type -t dnf5) ]]; then
+        yum=dnf5
+    else
+        # dnf5 does not support --color
+        flags+=( --color=never )
+        if [[ $(type -t dnf) ]]; then
+            yum=dnf
+        fi
     fi
-    declare flags=( -y --color=never )
     if [[ ! $install_debug ]]; then
         flags+=( -q )
     fi
-    install_info "$yum" "${args[@]}"
-    install_sudo "$yum" "${flags[@]}" "${args[@]}"
+    if [[ ${args[0]} != repolist ]]; then
+        install_info "$yum" "${args[@]}"
+    fi
+    # cat prevents color output in dnf5
+    install_sudo "$yum" "${flags[@]}" "${args[@]}" | cat
+}
+
+install_yum_add_repo() {
+    declare repo=$1
+    if [[ -r /etc/yum.repos.d/${repo##*/} ]]; then
+        return
+    fi
+    if [[ $(type -t dnf5) ]]; then
+        if [[ $(readlink /usr/bin/dnf) != dnf5 ]]; then
+            install_err 'dnf6 or above is not supported'
+        fi
+        install_yum_install dnf-plugins-core
+        install_yum addrepo --from-repofile="$repo"
+    elif [[ $(type -t dnf) ]]; then
+        # dnf 4 or before
+        install_yum_install dnf-plugins-core
+        install_yum config-manager --add-repo "$repo"
+    elif [[ $(type -t yum-config-manager) ]]; then
+        yum-config-manager --add-repo "$repo"
+        install_yum makecache fast
+    else
+        install_err "install_yum_add_repo does not support os=$install_os_release_id"
+    fi
 }
 
 install_yum_install() {
+    declare -a flags=()
+    while [[ ${1:-} =~ ^- ]]; do
+        flags+=( "$1" )
+        shift
+    done
     declare x y todo=()
     for x in "$@"; do
         y=$x
@@ -572,7 +663,7 @@ install_yum_install() {
     if (( ${#todo[@]} <= 0 )); then
         return
     fi
-    install_yum install "${todo[@]}"
+    install_yum install "${flags[@]}" "${todo[@]}"
 }
 
 install_main "$@"
