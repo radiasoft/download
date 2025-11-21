0211cae2fb5cee4812d8f785f98aa8ecc0126b4c
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-11-21 18:15:27 +0000

    building up to "ml"

--- installers/rpm-code/codes/boost.sh
+++ installers/rpm-code/codes/boost.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-_boost_ver=1.79.0
+# Fedora 43 has 1.83 but that doesn't compile:
+# libs/python/src/numpy/dtype.cpp:101:83: error: ‘PyArray_Descr’ {aka ‘struct _PyArray_Descr’} has no member named ‘elsize’
+_boost_ver=1.88.0
 
 _boost_dir=boost_${_boost_ver//./_}
 
 boost_python_install() {
     cd "$_boost_dir"
-    ./bootstrap.sh --prefix="${codes_dir[prefix]}"
-    local p="$(codes_python_include_dir)"
+    ./bootstrap.sh --prefix="${codes_dir[prefix]}" --with-libraries=python
+    declare p="$(codes_python_include_dir)"
     perl -pi -e "/using python/ && s{;}{: $p ;}" project-config.jam
     # libboost_numpy*.so* and libboost_python*.so* are installed in ~/.local/lib
     # not in ~/.pyenv. This is fine, because these are C++ libraries, and they have
@@ -18,6 +20,6 @@ boost_python_install() {
 boost_main() {
     codes_dependencies common
     codes_download \
-        "https://boostorg.jfrog.io/artifactory/main/release/$_boost_ver/source/$_boost_dir.tar.bz2" \
+        "https://archives.boost.io/release/$_boost_ver/source/$_boost_dir.tar.bz2" \
         '' boost "$_boost_ver"
 }
