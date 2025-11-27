3cf405f85d467459fde0219182389c117b7ce778
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-11-27 20:25:21 +0000

    petsc

--- installers/rpm-code/codes/petsc.sh
+++ installers/rpm-code/codes/petsc.sh
@@ -3,10 +3,8 @@
 petsc_main() {
     codes_yum_dependencies eigen3-devel bison
     codes_dependencies common boost parmetis hypre
-    local petsc_version=3.17.4
-    codes_download https://gitlab.com/petsc/petsc/-/archive/v"$petsc_version/petsc-v$petsc_version".tar.gz
-    perl -pi -e 's{((?:FCFLAGS|OPTF)\s*=)}{$1 -fallow-argument-mismatch }' config/BuildSystem/config/packages/{scalapack,MUMPS}.py
-    petsc_patch_configure
+    codes_download https://gitlab.com/petsc/petsc.git release
+    petsc_patch_spai
     ./configure --COPTFLAGS=-O2 --CXXOPTFLAGS=-O2 --FOPTFLAGS=-O2 \
         --with-fortran-bindings=no \
         --with-debugging=0 \
@@ -20,17 +18,13 @@ petsc_main() {
         --prefix="${codes_dir[prefix]}"
     codes_make
     codes_make_install
-    cd src/binding/petsc4py
+}
+
+petsc_python_install() {
+    cd petsc/src/binding/petsc4py
     PETSC_DIR="${codes_dir[prefix]}" codes_python_install
 }
 
-petsc_patch_configure() {
-    cat > configure <<EOF
-#!/usr/bin/env python3
-import sys, os
-
-sys.path.insert(0, os.path.abspath('config'))
-import configure
-configure.petsc_configure([])
-EOF
+petsc_patch_spai() {
+    perl -pi -e 's{(?<=CFLAGS = )}{-std=gnu99 }' config/BuildSystem/config/packages/spai.py
 }
