40414ccc9ef2584d93f2828798d77aec6085af0c
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-11-28 23:15:18 +0000

    not tested

--- installers/python-ci/radiasoft-download.sh
+++ installers/python-ci/radiasoft-download.sh
@@ -2,38 +2,51 @@
 #
 # Requires GITHUB_REPOSITORY and accepts GITHUB_TOKEN
 #
-python_ci_main() {
-    if ! [[ -r setup.py || -r pyproject.toml ]]; then
-        install_err 'no setup.py or pyproject.toml'
-    fi
-    declare i
-    declare -a p=()
-    declare r=$(basename "${GITHUB_REPOSITORY:-MISSING}")
-    case $r in
+
+python_ci_image() {
+    declare repo=$1
+    case $repo in
         pykern|chronver)
-            i=radiasoft/python3
+            _oci_image=radiasoft/python3
             ;;
         sirepo)
-            i=radiasoft/sirepo-ci
-            p+=( pykern )
+            _oci_image=radiasoft/sirepo-ci
+            _extra_pip+=( pykern )
             ;;
         MISSING)
             install_err '$GITHUB_REPOSITORY no set'
             ;;
         *)
-            p+=( pykern )
+            _extra_pip++=( pykern )
             if grep sirepo $(ls setup.py pyproject.toml 2>/dev/null) /dev/null &> /dev/null; then
-                i=radiasoft/sirepo
-                p+=( sirepo )
+                _oci_image=radiasoft/sirepo
+                _extra_pip+=( sirepo )
             else
-                i=radiasoft/python3
+                _oci_image=radiasoft/python3
             fi
             ;;
     esac
+    if [[ ${RADIA_RUN_PYTHON_CI_OCI_IMAGE:-} ]]; then
+        _oci_image=$RADIA_RUN_PYTHON_CI_OCI_IMAGE
+    fi
+    if [[ ! $_oci_image =~ : ]]; then
+        _oci_image+=:alpha
+    fi
+}
+
+python_ci_main() {
+    if ! [[ -r setup.py || -r pyproject.toml ]]; then
+        install_err 'no setup.py or pyproject.toml'
+    fi
+    declare i
+    declare r=$(basename "${GITHUB_REPOSITORY:-MISSING}")
     declare d=$PWD
     declare o=$(stat --format='%u:%g' "$d")
+    declare _oci_image
+    declare -a _extra_pip=()
+    python_ci_image "$r"
     set -x
-    $RADIA_RUN_OCI_CMD run -v "$d:$d" -i -u root --rm "$i:alpha" bash <<EOF | cat
+    $RADIA_RUN_OCI_CMD run -v "$d:$d" -i -u root --rm "$_oci_image" bash <<EOF | cat
         set -eou pipefail
         ${install_debug:+set -x}
         cd '$d'
@@ -48,7 +61,7 @@ python_ci_main() {
             # POSIT: no spaces or specials in repo names
             declare x
             _pip() { pip --disable-pip-version-check --no-color --quiet "\$@"; }
-            for x in ${p+${p[*]}}; do
+            for x in ${_extra_pip+${_extra_pip[*]}}; do
                 _pip uninstall -y \$x >& /dev/null || true
                 _pip install git+https://'${GITHUB_TOKEN:+$GITHUB_TOKEN@}'github.com/radiasoft/\$x.git
             done
