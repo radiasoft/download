ece0eb0735a4a1c84980f3f76eb3081e554a8735
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-12-02 22:55:15 +0000

    shadow

--- installers/rpm-code/codes/shadow3.sh
+++ installers/rpm-code/codes/shadow3.sh
@@ -7,7 +7,20 @@ shadow3_main() {
 shadow3_python_install() {
     # devel-gfortran-yb66 on 20220603
     # see also, xraylib.sh
-    install_pip_install srxraylib shadow3 xraylib
+    install_pip_install srxraylib xraylib
+    set -x
+    codes_download oasys-kit/shadow3 391b508deb3c4b24249482af1519a0084733b769
+    codes_download_module_file pyproject.toml
+    codes_download_module_file CMakeLists.txt
+    set -v
+    codes_download_module_file source.patch
+    rm -f setup.py
+    patch -p0 < source.patch
+    # build-system.requires doesn't seem to work for isolation
+    install_pip_install 'scikit-build-core>=0.10'
+    codes_python_install --no-build-isolation
+    # Don't need and might conflict
+    pip uninstall -y scikit-build-core
     install_pip_install --no-deps OASYS1-ShadowOui==1.5.229 SYNED==1.0.39 OASYS1-ShadowOui-Advanced-Tools==1.0.133
     local p=$(codes_python_lib_dir)/orangecontrib
     local u=$p/shadow/util
