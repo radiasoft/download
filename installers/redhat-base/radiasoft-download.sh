51a4afebe83818485cb1c14de94b4b94702c0704
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-12-11 20:16:54 +0000

    also save install_version_centos

--- installers/redhat-base/radiasoft-download.sh
+++ installers/redhat-base/radiasoft-download.sh
@@ -98,9 +98,10 @@ $(declare -f install_file_from_stdin)
 cat <<'END_CAT' | install_file_from_stdin 444 root root /etc/profile.d/rs-redhat-base.sh
 : \${RADIA_RUN_SERVER:='$install_server'}
 : \${install_depot_server:='$install_depot_server'}
+: \${install_version_centos:='$install_version_centos'}
 : \${install_version_fedora:='$install_version_fedora'}
 : \${install_version_python:='$install_version_python'}
-export RADIA_RUN_SERVER install_depot_server install_version_fedora install_version_python
+export RADIA_RUN_SERVER install_depot_server install_version_centos install_version_fedora install_version_python
 END_CAT
 END_SUDO
 }
