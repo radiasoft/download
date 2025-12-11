c1aca27478490e57324ffb69f13fcfaabe4b73d8
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-12-11 23:44:42 +0000

    parmetis: fix build

--- installers/rpm-code/codes/parmetis.sh
+++ installers/rpm-code/codes/parmetis.sh
@@ -2,10 +2,13 @@
 
 
 parmetis_main() {
+    echo "${codes_dir[prefix]}"
     codes_dependencies common
     for f in GKlib METIS ParMETIS; do
         codes_download KarypisLab/"$f"
-        make config prefix="${codes_dir[prefix]}"
+        codes_cmake_fix_lib_dir
+        # gklib_path is hardwired incorrectly in Makefile
+        make config prefix="${codes_dir[prefix]}" gklib_path="${codes_dir[prefix]}"
         codes_make_install
         cd ..
     done
