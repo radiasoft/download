5de6b2ad16ba65de0e249428609ebf35ea3ef1c8
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-12-28 17:51:23 +0000

    Fix #844 xraylib needs explicit --enable-python

--- installers/rpm-code/codes/xraylib.sh
+++ installers/rpm-code/codes/xraylib.sh
@@ -6,11 +6,12 @@ xraylib_python_install() {
     ./configure --prefix="${codes_dir[prefix]}"\
       --disable-idl --disable-java \
       --disable-lua --disable-perl --disable-libtool-lock --disable-ruby \
-      --disable-php --enable-python-integration
+      --disable-php --enable-python-integration --enable-python --enable-python-numpy
     codes_make
     codes_make_install
 }
 
 xraylib_main() {
+    codes_yum_dependencies swig
     codes_dependencies common
 }
