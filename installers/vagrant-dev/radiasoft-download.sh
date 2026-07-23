27be747a0805945dfb2a0e9d893cd110625a821b
Justin Schell <jschell@bivio.biz>
2026-07-23 22:54:44 +0000

    make backward compatible

--- installers/vagrant-dev/radiasoft-download.sh
+++ installers/vagrant-dev/radiasoft-download.sh
@@ -26,9 +26,9 @@ vagrant_dev_box_add() {
         box=generic/fedora$install_version_fedora
     elif [[ $box == centos ]]; then
         if [[ $_vagrant_dev_host_os == ubuntu ]]; then
-            box=generic/centos$install_version_rhel
+            box=generic/centos${install_version_rhel:-install_version_centos}
         else
-            box=centos/$install_version_rhel
+            box=centos/${install_version_rhel:-install_version_centos}
         fi
     elif [[ $box == almalinux ]]; then
         if [[ $_vagrant_dev_host_os == ubuntu ]]; then
